09b824eb71ed62d2bd23b914be8ade551d63cc14
Hapnees <8dwardm8lton@gmail.com>
2023-04-16 22:24:13 +0300

    Feat (UI): add LongButton

---
+++ src/components/UI/LongButton/LongButton.tsx
@@ -0,0 +1,32 @@
+import { IVarButton } from '@/types/button.interface'
+import Image from 'next/image'
+import { ButtonHTMLAttributes, DetailedHTMLProps, FC } from 'react'
+import style from './LongButton.module.scss'
+
+interface IProps
+	extends DetailedHTMLProps<
+		ButtonHTMLAttributes<HTMLButtonElement>,
+		HTMLButtonElement
+	> {
+	variant?: IVarButton
+	img?: string
+}
+
+const LongButton: FC<IProps> = ({
+	variant = 'primary',
+	img,
+	children,
+	...props
+}) => {
+	const additClassName = variant === 'secondary' ? style.secondary : ''
+	const className = `${style.button} ${additClassName}`
+
+	return (
+		<button className={className} {...props}>
+			{img && <Image src={img} alt='' width={24} height={32} />}
+			<p>{children}</p>
+		</button>
+	)
+}
+
+export default LongButton
---
+++ src/types/button.interface.ts
@@ -0,0 +1,1 @@
+export type IVarButton = 'primary' | 'secondary'
