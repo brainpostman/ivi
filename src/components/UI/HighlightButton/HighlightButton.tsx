3010bb66e689dcd4e40d17a70f52d15af31eb138
Hapnees <8dwardm8lton@gmail.com>
2023-04-17 18:34:15 +0300

    Feat (HighlightButton): remove prop @variant

--- src/components/UI/HighlightButton/HighlightButton.tsx
+++ src/components/UI/HighlightButton/HighlightButton.tsx
@@ -1,25 +1,11 @@
-import { IVarButton } from '@/types/button.interface'
 import { ButtonHTMLAttributes, DetailedHTMLProps, FC } from 'react'
 import style from './HighlightButton.module.scss'
 
-interface IProps
-	extends DetailedHTMLProps<
-		ButtonHTMLAttributes<HTMLButtonElement>,
-		HTMLButtonElement
-	> {
-	variant?: IVarButton
-}
-
-const HighlightButton: FC<IProps> = ({
-	children,
-	variant = 'primary',
-	...props
-}) => {
-	const additClassName = variant === 'secondary' ? style.secondary : ''
-	const className = `${style.button} ${additClassName}`
-
+const HighlightButton: FC<
+	DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+> = ({ children, ...props }) => {
 	return (
-		<button className={className} {...props}>
+		<button className={style.button} {...props}>
 			{children}
 		</button>
 	)
