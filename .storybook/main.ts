a5e5af3efed80060bcd4f08d5651b1c30445bb4d
Hapnees <8dwardm8lton@gmail.com>
2023-04-17 15:10:14 +0300

    Feat (root): add storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,41 +1,41 @@
 import type { StorybookConfig } from '@storybook/nextjs'
 import path from 'path'
 const config: StorybookConfig = {
-	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
-	],
-	webpackFinal: async (config, { configType }) => {
-		if (config.resolve)
-			config.resolve.alias = {
-				...config.resolve.alias,
-				'@': path.resolve(__dirname, '../src/'),
-			}
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  webpackFinal: async (config, { configType }) => {
+    if (config.resolve)
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@': path.resolve(__dirname, '../src/'),
+      }
 
-		return config
-	},
-	typescript: {
-		check: false,
-		checkOptions: {},
-		reactDocgen: 'react-docgen-typescript',
-		reactDocgenTypescriptOptions: {
-			shouldRemoveUndefinedFromOptional: true,
-			shouldExtractLiteralValuesFromEnum: true,
-			propFilter: prop =>
-				prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
-		},
-	},
-	framework: {
-		name: '@storybook/nextjs',
-		options: {},
-	},
-	core: {
-		disableTelemetry: true,
-	},
-	docs: {
-		autodocs: 'tag',
-	},
+    return config
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldRemoveUndefinedFromOptional: true,
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: prop =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+  docs: {
+    autodocs: 'tag',
+  },
 }
 export default config
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,26 +3,26 @@ import DocsTemplate from '../src/stories/DocsTemplate.mdx'
 import '../src/styles/globals.scss'
 
 const preview: Preview = {
-	parameters: {
-		backgrounds: {
-			default: 'dark',
-			values: [
-				{ name: 'dark', value: '#100e19' },
-				{ name: 'darkSoft', value: '#353147' },
-				{ name: 'light', value: '#ffffff' },
-			],
-		},
-		docs: {
-			page: DocsTemplate,
-		},
-		actions: { argTypesRegex: '^on[A-Z].*' },
-		controls: {
-			matchers: {
-				color: /(background|color)$/i,
-				date: /Date$/,
-			},
-		},
-	},
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: '#100e19' },
+        { name: 'darkSoft', value: '#353147' },
+        { name: 'light', value: '#ffffff' },
+      ],
+    },
+    docs: {
+      page: DocsTemplate,
+    },
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
 }
 
 export default preview
