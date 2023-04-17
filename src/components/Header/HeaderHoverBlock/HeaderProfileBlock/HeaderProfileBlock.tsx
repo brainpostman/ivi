f45aad88a42dad01cba83393af42098b7b7e81e5
Hapnees <8dwardm8lton@gmail.com>
2023-04-17 14:46:48 +0300

    Feat (HeaderHoverBlock): add HeaderProfileBlock

---
+++ src/components/Header/HeaderHoverBlock/HeaderProfileBlock/HeaderProfileBlock.tsx
@@ -0,0 +1,64 @@
+import HighlightButton from '@/components/UI/HighlightButton/HighlightButton'
+import { BiChalkboard, BiPurchaseTagAlt } from 'react-icons/bi'
+import { BsFillBookmarkFill, BsFillCreditCardFill } from 'react-icons/bs'
+import { FaHistory } from 'react-icons/fa'
+import { RiShareLine } from 'react-icons/ri'
+import { SlDiamond } from 'react-icons/sl'
+import { TbCertificate } from 'react-icons/tb'
+import style from './HeaderProfileBlock.module.scss'
+
+const HeaderProfileBlock = () => {
+	return (
+		<div className={style.wrapper}>
+			<ul className={style.list}>
+				<li>
+					<BiPurchaseTagAlt className={style.icon} />
+					<p className={style.title}>Покупки</p>
+				</li>
+				<li>
+					<BsFillBookmarkFill className={style.icon} />
+					<p className={style.title}>Смотреть позже</p>
+				</li>
+				<li>
+					<FaHistory className={style.icon} />
+					<p className={style.title}>История просмотров</p>
+				</li>
+				<li>
+					<div className={style.circle}></div>
+					<SlDiamond className={style.icon} />
+					<div>
+						<p className={style.title}>Подписки</p>
+						<p className={style.subtitle}>Подключить</p>
+					</div>
+				</li>
+				<li>
+					<TbCertificate className={style.icon} />
+					<p className={style.title}>Активация сертификата</p>
+				</li>
+				<li>
+					<BiChalkboard className={style.icon} />
+					<p className={style.title}>Вход по коду</p>
+				</li>
+				<li>
+					<BsFillCreditCardFill className={style.icon} />
+					<p className={style.title}>Способ оплаты</p>
+				</li>
+				<li>
+					<RiShareLine className={style.icon} />
+					<p className={style.title}>Пригласить друзей</p>
+				</li>
+			</ul>
+
+			<div className={style.rightSide}>
+				<HighlightButton>Войти или зарегистрироваться</HighlightButton>
+
+				<div className={style.labels}>
+					<p className='text'>Настройки</p>
+					<p className='text'>Помощь</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default HeaderProfileBlock
