cbde61c1e5a51c403cf0fc127a531d948e172767
Hapnees <8dwardm8lton@gmail.com>
2023-04-19 01:22:48 +0300

    Feat (PageLayout): add section-container

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -1,29 +1,32 @@
-import Footer from '@/components/Footer/Footer';
-import HeadModif from '@/components/HeadModif/HeadModif';
-import Header from '@/components/Header/Header';
-import { IHead } from '@/types/head.interface';
-import { FC, PropsWithChildren } from 'react';
+import Footer from '@/components/Footer/Footer'
+import HeadModif from '@/components/HeadModif/HeadModif'
+import Header from '@/components/Header/Header'
+import { IHead } from '@/types/head.interface'
+import { FC, PropsWithChildren } from 'react'
+import style from './PageLayout.module.scss'
 
 const PageLayout: FC<PropsWithChildren<IHead>> = ({
-    title,
-    description,
-    keywords,
-    noIndex,
-    children,
+	title,
+	description,
+	keywords,
+	noIndex,
+	children,
 }) => {
-    return (
-        <>
-            <HeadModif
-                title={title}
-                description={description}
-                keywords={keywords}
-                noIndex={noIndex}
-            />
-            <Header />
-            <main>{children}</main>
-            <Footer />
-        </>
-    );
-};
+	return (
+		<>
+			<HeadModif
+				title={title}
+				description={description}
+				keywords={keywords}
+				noIndex={noIndex}
+			/>
+			<Header />
+			<main>
+				<section className={style.container}>{children}</section>
+			</main>
+			<Footer />
+		</>
+	)
+}
 
-export default PageLayout;
+export default PageLayout
