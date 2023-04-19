387f318992d46ccc363682940f119f4c77c366f2
Hapnees <8dwardm8lton@gmail.com>
2023-04-19 23:07:14 +0300

    Feat(MainCarousel) remove extra values

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,17 @@
-import MainCarousel from '@/components/MainCarousel/MainCarousel';
-import mainCarouselData from '@/data/main_carousel/mainCarouselData';
-import PageLayout from '@/layouts/PageLayout';
+import MainCarousel from '@/components/MainCarousel/MainCarousel'
+import mainCarouselData from '@/data/main_carousel/mainCarouselData'
+import PageLayout from '@/layouts/PageLayout'
 
 const title =
-    'Онлайн-кинотеатр Иви - фильмы, сериалы и мультфильмы смотреть онлайн бесплатно в хорошем качестве';
+	'Онлайн-кинотеатр Иви - фильмы, сериалы и мультфильмы смотреть онлайн бесплатно в хорошем качестве'
 
 const description =
-    'Устройте кинотеатр у себя дома! Смотрите онлайн фильмы хорошего качества в приятной домашней обстановке и в удобное для вас время. Для вас всегда доступны бесплатные фильмы без регистрации на любой вкус: сериалы, фильмы, мультфильмы и многое другое.';
+	'Устройте кинотеатр у себя дома! Смотрите онлайн фильмы хорошего качества в приятной домашней обстановке и в удобное для вас время. Для вас всегда доступны бесплатные фильмы без регистрации на любой вкус: сериалы, фильмы, мультфильмы и многое другое.'
 
 export default function Home() {
-    return (
-        <PageLayout title={title} description={description}>
-            <MainCarousel
-                items={mainCarouselData}
-                speed={800}
-                autoScroll={true}
-                autoscrollSpeed={5000}
-            />
-        </PageLayout>
-    );
+	return (
+		<PageLayout title={title} description={description}>
+			<MainCarousel items={mainCarouselData} speed={800} />
+		</PageLayout>
+	)
 }
