86f46ce775d5499a1b0dbfb46384300aa9f36c7f
Hapnees <8dwardm8lton@gmail.com>
2023-04-19 14:45:20 +0300

    Feat (UI): rework BasicBtn

--- src/components/Footer/FooterSocials/FooterSocials.tsx
+++ src/components/Footer/FooterSocials/FooterSocials.tsx
@@ -1,49 +1,41 @@
-import BasicBtn from '@/components/UI/BasicBtn/BasicBtn';
-import styles from './FooterSocials.module.scss';
+import BasicBtn from '@/components/UI/BasicBtn/BasicBtn'
 import {
-    FaVk,
-    FaOdnoklassniki,
-    FaTwitter,
-    FaViber,
-    FaLinkedinIn,
-    FaTelegramPlane,
-} from 'react-icons/fa';
+	FaLinkedinIn,
+	FaOdnoklassniki,
+	FaTelegramPlane,
+	FaTwitter,
+	FaViber,
+	FaVk,
+} from 'react-icons/fa'
+import styles from './FooterSocials.module.scss'
 
 const FooterSocials = () => {
-    return (
-        <div className={styles.container}>
-            <a href='https://vk.com/iviru' target='_blank' rel='noreferrer'>
-                <BasicBtn btnType={'icon'} variant={2}>
-                    <FaVk className={styles.icon} />
-                </BasicBtn>
-            </a>
-            <a href='https://ok.ru/ivi.ru' target='_blank' rel='noreferrer'>
-                <BasicBtn btnType={'icon'} variant={2}>
-                    <FaOdnoklassniki className={styles.icon} />
-                </BasicBtn>
-            </a>
-            <a href='https://twitter.com/ivi_ru' target='_blank' rel='noreferrer'>
-                <BasicBtn btnType={'icon'} variant={2}>
-                    <FaTwitter className={styles.icon} />
-                </BasicBtn>
-            </a>
-            <a href='https://vb.me/a0544c' target='_blank' rel='noreferrer'>
-                <BasicBtn btnType={'icon'} variant={2}>
-                    <FaViber className={styles.icon} />
-                </BasicBtn>
-            </a>
-            <a href='https://www.linkedin.com/company/2543415/' target='_blank' rel='noreferrer'>
-                <BasicBtn btnType={'icon'} variant={2}>
-                    <FaLinkedinIn className={styles.icon} />
-                </BasicBtn>
-            </a>
-            <a href='https://t.me/official_iviru' target='_blank' rel='noreferrer'>
-                <BasicBtn btnType={'icon'} variant={2}>
-                    <FaTelegramPlane className={styles.icon} />
-                </BasicBtn>
-            </a>
-        </div>
-    );
-};
+	return (
+		<div className={styles.container}>
+			<BasicBtn btnType='iconCircle' href='https://vk.com/iviru'>
+				<FaVk className={styles.icon} />
+			</BasicBtn>
 
-export default FooterSocials;
+			<BasicBtn btnType='iconCircle' href='https://ok.ru/ivi.ru'>
+				<FaOdnoklassniki className={styles.icon} />
+			</BasicBtn>
+			<BasicBtn btnType='iconCircle' href='https://twitter.com/ivi_ru'>
+				<FaTwitter className={styles.icon} />
+			</BasicBtn>
+			<BasicBtn btnType='iconCircle' href='https://vb.me/a0544c'>
+				<FaViber className={styles.icon} />
+			</BasicBtn>
+			<BasicBtn
+				btnType='iconCircle'
+				href='https://www.linkedin.com/company/2543415/'
+			>
+				<FaLinkedinIn className={styles.icon} />
+			</BasicBtn>
+			<BasicBtn btnType='iconCircle' href='https://t.me/official_iviru'>
+				<FaTelegramPlane className={styles.icon} />
+			</BasicBtn>
+		</div>
+	)
+}
+
+export default FooterSocials
--- src/components/UI/BasicBtn/BasicBtn.tsx
+++ src/components/UI/BasicBtn/BasicBtn.tsx
@@ -1,32 +1,73 @@
-import typeText from './TextBasicBtn.module.scss';
-import typeIcon from './IconBasicBtn.module.scss';
+import { IBtnType } from '@/types/button.interface'
+import { FC, PropsWithChildren } from 'react'
+import style from './BasicBtn.module.scss'
 
 export interface IBasicBtnProps
-    extends React.DetailedHTMLProps<
-        React.ButtonHTMLAttributes<HTMLButtonElement>,
-        HTMLButtonElement
-    > {
-    btnType: 'text' | 'icon';
-    variant: 1 | 2;
+	extends React.DetailedHTMLProps<
+		React.ButtonHTMLAttributes<HTMLButtonElement>,
+		HTMLButtonElement
+	> {
+	btnType: IBtnType
+	href?: string
+	suptitle?: string
+	title?: string
+}
+
+const getClassNameBtnType = (btnType: IBtnType) => {
+	switch (btnType) {
+		case 'textPlusIcon':
+			return style.textPlusIcon
+		case 'icon':
+			return style.icon
+		case 'iconCircle':
+			return style.iconCircle
+		default:
+			return style.text
+	}
 }
 
 const BasicBtn = ({
-    className = '',
-    btnType = 'text',
-    variant = 1,
-    children,
-    ...props
+	className = '',
+	btnType = 'text',
+	children,
+	title,
+	suptitle,
+	href,
+	...props
 }: IBasicBtnProps) => {
-    let styles = btnType === 'text' ? typeText : typeIcon;
-    return (
-        <button
-            className={`${styles.button} ${
-                variant === 1 ? styles.variant1 : styles.variant2
-            } ${className}`}
-            {...props}>
-            {children}
-        </button>
-    );
-};
+	const classNameBtnType = getClassNameBtnType(btnType)
 
-export default BasicBtn;
+	const WrapperButton: FC<PropsWithChildren<unknown>> = ({ children }) =>
+		href ? (
+			<a href={href} target='_blank' rel='noreferrer' className={className}>
+				{children}
+			</a>
+		) : (
+			<>{children}</>
+		)
+
+	return (
+		<WrapperButton>
+			<button
+				className={`${style.button} ${classNameBtnType} ${
+					href ? '' : className
+				}`}
+				{...props}
+			>
+				{btnType === 'textPlusIcon' ? (
+					<>
+						{children}
+						<div className={style.description}>
+							{suptitle && <p className={style.suptitle}>{suptitle}</p>}
+							<p className={style.title}>{title}</p>
+						</div>
+					</>
+				) : (
+					children
+				)}
+			</button>
+		</WrapperButton>
+	)
+}
+
+export default BasicBtn
