b7a5fe51b65d291f237c3c664e66308c144aee2e
Hapnees <8dwardm8lton@gmail.com>
2023-04-20 14:49:52 +0300

    Feat (components): rework TopTenList

---
+++ src/data/topTenList.data.ts
@@ -0,0 +1,47 @@
+interface Film {
+	title: string
+	img: string
+}
+
+export const topTenListData: Film[] = [
+	{
+		title: 'фильм 1',
+		img: 'https://thumbs.dfs.ivi.ru/storage26/contents/a/1/56b0cedeb8dd5890ee46ab947c1207.jpg/304x620//?q=85',
+	},
+	{
+		title: 'фильм 2',
+		img: 'https://thumbs.dfs.ivi.ru/storage28/contents/d/e/f3afc43a0709ea1ebf35cdf142cc46.jpg/304x620//?q=85',
+	},
+	{
+		title: 'фильм 3',
+		img: 'https://thumbs.dfs.ivi.ru/storage6/contents/e/0/08c76a2df45108ec28af6300cc5b93.jpg/304x620//?q=85',
+	},
+	{
+		title: 'фильм 4',
+		img: 'https://thumbs.dfs.ivi.ru/storage28/contents/f/2/7e50d51661b729863f8584ee559242.jpg/304x620//?q=85',
+	},
+	{
+		title: 'фильм 5',
+		img: 'https://thumbs.dfs.ivi.ru/storage15/contents/2/2/b73c42810f30fbd1a5fe65d2bebc38.jpg/304x620//?q=85',
+	},
+	{
+		title: 'фильм 6',
+		img: 'https://thumbs.dfs.ivi.ru/storage3/contents/0/9/09f0b7761bcd8ceb9c42bb93f4806c.jpg/304x620//?q=85',
+	},
+	{
+		title: 'фильм 7',
+		img: 'https://thumbs.dfs.ivi.ru/storage5/contents/9/9/f92d6d3965cfcb7b7e1878eaa56f05.jpg/304x620//?q=85',
+	},
+	{
+		title: 'фильм 8',
+		img: 'https://thumbs.dfs.ivi.ru/storage26/contents/a/1/56b0cedeb8dd5890ee46ab947c1207.jpg/304x620//?q=85',
+	},
+	{
+		title: 'фильм 9',
+		img: 'https://thumbs.dfs.ivi.ru/storage33/contents/b/2/661be4b1a65157bb5440ffc3bbeb06.jpg/304x620//?q=85',
+	},
+	{
+		title: 'фильм 10',
+		img: 'https://thumbs.dfs.ivi.ru/storage4/contents/2/c/3b8e2a957b926355725e1237e188bf.jpg//304x620//?q=85',
+	},
+]
