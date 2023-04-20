a67e94c69ae1b63aa44f3c75f007d59e168008fa
Marat Sattarov <sattarovmk@yandex.ru>
2023-04-20 17:02:16 +0500

    Feat (component) fixed styles on BasicBtn, Footer

--- src/components/Footer/FooterDevices/FooterDevices.tsx
+++ src/components/Footer/FooterDevices/FooterDevices.tsx
@@ -1,44 +1,40 @@
-import BasicBtn from '@/components/UI/BasicBtn/BasicBtn'
-import { BiDevices } from 'react-icons/bi'
-import { FaApple, FaGooglePlay, FaTv } from 'react-icons/fa'
-import styles from './FooterDevices.module.scss'
+import BasicBtn from '@/components/UI/BasicBtn/BasicBtn';
+import { BiDevices } from 'react-icons/bi';
+import { FaApple, FaGooglePlay, FaTv } from 'react-icons/fa';
+import styles from './FooterDevices.module.scss';
 
 const FooterDevices = () => {
-	return (
-		<div className={styles.container}>
-			<BasicBtn
-				btnType='textPlusIcon'
-				href='https://apps.apple.com/RU/app/id455705533'
-				title='App Store'
-				suptitle='Загрузить в'
-			>
-				<FaApple />
-			</BasicBtn>
-			<BasicBtn
-				btnType='textPlusIcon'
-				href='https://play.google.com/store/apps/details?id=ru.ivi.client'
-				title='Google Play'
-				suptitle='Доступно в'
-			>
-				<FaGooglePlay />
-			</BasicBtn>
-			<BasicBtn
-				btnType='textPlusIcon'
-				href='https://www.ivi.ru/pages/tvsmart/'
-				title='Smart TV'
-				suptitle='Смотрите на'
-			>
-				<FaTv />
-			</BasicBtn>
-			<BasicBtn
-				btnType='textPlusIcon'
-				href='https://www.ivi.ru/devices'
-				title='Все устройства'
-			>
-				<BiDevices />
-			</BasicBtn>
-		</div>
-	)
-}
+    return (
+        <div className={styles.container}>
+            <BasicBtn
+                btnType='textPlusIcon'
+                href='https://apps.apple.com/RU/app/id455705533'
+                title='App Store'
+                suptitle='Загрузить в'>
+                <FaApple />
+            </BasicBtn>
+            <BasicBtn
+                btnType='textPlusIcon'
+                href='https://play.google.com/store/apps/details?id=ru.ivi.client'
+                title='Google Play'
+                suptitle='Доступно в'>
+                <FaGooglePlay />
+            </BasicBtn>
+            <BasicBtn
+                btnType='textPlusIcon'
+                href='https://www.ivi.ru/pages/tvsmart/'
+                title='Smart TV'
+                suptitle='Смотрите на'>
+                <FaTv />
+            </BasicBtn>
+            <BasicBtn
+                btnType='textPlusIcon'
+                href='https://www.ivi.ru/devices'
+                title='Все устройства'>
+                <BiDevices />
+            </BasicBtn>
+        </div>
+    );
+};
 
-export default FooterDevices
+export default FooterDevices;
--- src/components/Footer/FooterSupport/FooterSupport.tsx
+++ src/components/Footer/FooterSupport/FooterSupport.tsx
@@ -1,47 +1,46 @@
-import BasicBtn from '@/components/UI/BasicBtn/BasicBtn'
-import { useState } from 'react'
-import { FiPhone } from 'react-icons/fi'
-import { GoMail } from 'react-icons/go'
-import styles from './FooterSupport.module.scss'
+import BasicBtn from '@/components/UI/BasicBtn/BasicBtn';
+import { useState } from 'react';
+import { FiPhone } from 'react-icons/fi';
+import { GoMail } from 'react-icons/go';
+import styles from './FooterSupport.module.scss';
 
 const FooterSupport = () => {
-	const [showPopUp, setShowPopUp] = useState(false)
+    const [showPopUp, setShowPopUp] = useState(false);
 
-	return (
-		<div className={styles.container}>
-			<h3 className={styles.title}>Служба поддержки</h3>
-			<div className={styles.description}>
-				<p>Мы всегда готовы вам помочь.</p>
-				<p>Наши операторы онлайн 24/7</p>
-			</div>
-			<BasicBtn btnType='text' href='https://www.ivi.ru/profile'>
-				Написать в чате
-			</BasicBtn>
-			<div className={styles.buttons}>
-				<BasicBtn btnType='icon' href='mailto:support@ivi.ru'>
-					<GoMail />
-				</BasicBtn>
+    return (
+        <div className={styles.container}>
+            <h3 className={styles.title}>Служба поддержки</h3>
+            <div className={styles.description}>
+                <p>Мы всегда готовы вам помочь.</p>
+                <p>Наши операторы онлайн 24/7</p>
+            </div>
+            <BasicBtn btnType='text' href='https://www.ivi.ru/profile'>
+                Написать в чате
+            </BasicBtn>
+            <div className={styles.buttons}>
+                <BasicBtn btnType='icon' href='mailto:support@ivi.ru'>
+                    <GoMail />
+                </BasicBtn>
 
-				<BasicBtn btnType='icon' onClick={() => setShowPopUp(prev => !prev)}>
-					<FiPhone />
-				</BasicBtn>
+                <BasicBtn btnType='icon' onClick={() => setShowPopUp((prev) => !prev)}>
+                    <FiPhone />
+                </BasicBtn>
 
-				<BasicBtn
-					className={`${styles.popup} ${showPopUp ? styles.popupActive : ''}`}
-					href='tel:+78442459825'
-					btnType='text'
-				>
-					+7 347 258-80-05
-				</BasicBtn>
-			</div>
-			<div className={styles.askivi}>
-				<a href='https://ask.ivi.ru/' target='_blank' rel='noreferrer'>
-					ask.ivi.ru
-				</a>
-				<p>Ответы на вопросы</p>
-			</div>
-		</div>
-	)
-}
+                <BasicBtn
+                    className={`${styles.popup} ${showPopUp ? styles.popupActive : ''}`}
+                    href='tel:+78442459825'
+                    btnType='text'>
+                    +7 347 258-80-05
+                </BasicBtn>
+            </div>
+            <div className={styles.askivi}>
+                <a href='https://ask.ivi.ru/' target='_blank' rel='noreferrer'>
+                    ask.ivi.ru
+                </a>
+                <p>Ответы на вопросы</p>
+            </div>
+        </div>
+    );
+};
 
-export default FooterSupport
+export default FooterSupport;
