50910723bb2d76317d8fbeff8df686dbac1c6a70
Marat Sattarov <sattarovmk@yandex.ru>
2023-04-22 02:01:13 +0500

    Feat (AuthModal/Header) modal bool flag handlers

--- src/components/AuthModal/AuthModal.tsx
+++ src/components/AuthModal/AuthModal.tsx
@@ -1,16 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './AuthModal.module.scss';
-import { RiCloseFill } from 'react-icons/ri';
+import { useTypedDispatch } from '@/hooks/ReduxHooks';
+import { setAuthModal } from '../../store/reducers/authModalReducer';
 
 interface IAuthModalProps {
     modalShown: boolean;
-    closeModal: () => void;
 }
 
-const AuthModal = ({ modalShown, closeModal }: IAuthModalProps) => {
+const AuthModal = ({ modalShown }: IAuthModalProps) => {
     const [progressBar, setProgressBar] = useState(5);
+    const dispatch = useTypedDispatch();
+    const modalRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
+        if (modalRef.current) {
+            modalRef.current.style.top = window.scrollY + 'px';
+        }
         document.body.style.overflow = 'hidden';
         return () => {
             document.body.style.overflow = 'unset';
@@ -18,24 +23,22 @@ const AuthModal = ({ modalShown, closeModal }: IAuthModalProps) => {
     }, [modalShown]);
 
     return (
-        <>
-            {modalShown && (
-                <div className={styles.modal}>
-                    <section className={styles.header}>
-                        <div className={styles.header__container}>
-                            <div></div>
-                            <h2 className={styles.header__title}>Вход или регистрация</h2>
-                            <div className={styles.close} onClick={() => closeModal()}></div>
-                        </div>
-                        <div
-                            className={styles.header__progressBar}
-                            style={{
-                                width: `${progressBar}%`,
-                            }}></div>
-                    </section>
+        <div className={styles.modal} ref={modalRef}>
+            <section className={styles.header}>
+                <div className={styles.header__container}>
+                    <div></div>
+                    <h2 className={styles.header__title}>Вход или регистрация</h2>
+                    <div
+                        className={styles.close}
+                        onClick={() => dispatch(setAuthModal(false))}></div>
                 </div>
-            )}
-        </>
+                <div
+                    className={styles.header__progressBar}
+                    style={{
+                        width: `${progressBar}%`,
+                    }}></div>
+            </section>
+        </div>
     );
 };
 
