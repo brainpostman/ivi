154f2cdaeb426ec16405b7f30a2139b83cc65e40
Hapnees <8dwardm8lton@gmail.com>
2023-04-22 13:06:16 +0300

    Feat (data): add filterDatas

---
+++ src/data/filterCountry.data.ts
@@ -0,0 +1,58 @@
+import { IVarButton } from '@/types/button.interface'
+
+interface IFilterCountryDataEl {
+	children: string
+	variant?: IVarButton
+}
+
+export const filterCountryData: IFilterCountryDataEl[] = [
+	{ children: 'Россия', variant: 'secondary' },
+	{ children: 'США', variant: 'secondary' },
+	{ children: 'Великобритания', variant: 'secondary' },
+	{ children: 'Франция', variant: 'secondary' },
+	{ children: 'СССР', variant: 'secondary' },
+	{ children: 'Италия', variant: 'secondary' },
+	{ children: 'Канада', variant: 'secondary' },
+	{ children: 'Испания', variant: 'secondary' },
+	{ children: 'Германия', variant: 'secondary' },
+	{ children: 'Индия', variant: 'secondary' },
+]
+
+export const filterCountryListData = [
+	'Австралия',
+	'Беларусь',
+	'Великобритания',
+	'Гонконг',
+	'Ирландия',
+	'Казахстан',
+	'Колумбия',
+	'Новая Зеландия',
+	'Россия',
+	'Таиланд',
+	'Франция',
+	'ЮАР',
+	'Аргентина',
+	'Бельгия',
+	'Венгрия',
+	'Дания',
+	'Испания',
+	'Канада',
+	'Мексика',
+	'Норвегия',
+	'СССР',
+	'Турция',
+	'Швейцария',
+	'Южная Корея',
+	'Армения',
+	'Бразилия',
+	'Германия',
+	'Индия',
+	'Италия',
+	'Китай',
+	'Нидерланды',
+	'Польша',
+	'США',
+	'Финляндия',
+	'Швеция',
+	'Япония',
+]
---
+++ src/data/filterGenre.data.tsx
@@ -0,0 +1,54 @@
+import { BsFillHeartbreakFill } from 'react-icons/bs'
+import {
+	GiCrossedPistols,
+	GiDramaMasks,
+	GiGhost,
+	GiMagickTrick,
+	GiTreasureMap,
+} from 'react-icons/gi'
+import { MdFamilyRestroom, MdTheaterComedy } from 'react-icons/md'
+import { RiAliensLine } from 'react-icons/ri'
+import { TfiWorld } from 'react-icons/tfi'
+
+export const filterGenreData = [
+	{ title: 'Драмы', icon: <GiDramaMasks /> },
+	{ title: 'Комедии', icon: <MdTheaterComedy /> },
+	{ title: 'Боевики', icon: <GiCrossedPistols /> },
+	{ title: 'Триллеры', icon: <GiGhost /> },
+	{ title: 'Приключения', icon: <GiTreasureMap /> },
+	{ title: 'Зарубежные', icon: <TfiWorld /> },
+	{ title: 'Мелодрамы', icon: <BsFillHeartbreakFill /> },
+	{ title: 'Фантастика', icon: <RiAliensLine /> },
+	{ title: 'Фэнтези', icon: <GiMagickTrick /> },
+	{ title: 'Семейные', icon: <MdFamilyRestroom /> },
+]
+
+export const filterGenreListData = [
+	'Артхаус',
+	'Вестерн',
+	'Для детей',
+	'Зарубежные',
+	'Комедии',
+	'Мистические',
+	'Приключения',
+	'Советские',
+	'Ужасы',
+	'Биография',
+	'Военные',
+	'Документальные',
+	'Исторические',
+	'Криминал',
+	'Музыкальные',
+	'Русские',
+	'Спорт',
+	'Фантастика',
+	'Боевик',
+	'Детективы',
+	'Драмы',
+	'Катастрофы',
+	'Мелодрамы',
+	'По комиксам',
+	'Семейные',
+	'Триллеры',
+	'Фэнтези',
+]
