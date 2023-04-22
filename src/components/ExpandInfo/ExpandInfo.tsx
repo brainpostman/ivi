801d49db391c346f7c59238cc2f934adf469ec76
Hapnees <8dwardm8lton@gmail.com>
2023-04-22 13:01:53 +0300

    Feat (components): add ExpandInfo

---
+++ src/components/ExpandInfo/ExpandInfo.tsx
@@ -0,0 +1,38 @@
+import { DetailedHTMLProps, FC, HTMLAttributes, useState } from 'react'
+import style from './ExpandInfo.module.scss'
+
+interface IProps
+	extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
+	title?: string
+	visibleText?: string
+}
+
+const ExpandInfo: FC<IProps> = ({ children, title, visibleText, ...props }) => {
+	const [isExpand, setIsExpand] = useState(false)
+
+	const onClickToggle = () => setIsExpand(prev => !prev)
+
+	return (
+		<article className={style.info} {...props}>
+			{title && <h1 className={style.info__title}>{title}</h1>}
+
+			{visibleText && (
+				<p
+					className={`${style.info__body__top} ${
+						isExpand ? style.visible : ''
+					}`}
+				>
+					{visibleText}
+				</p>
+			)}
+
+			{isExpand && children}
+
+			<p className={style.toggle} onClick={onClickToggle}>
+				{isExpand ? 'Свернуть' : 'Развернуть'}
+			</p>
+		</article>
+	)
+}
+
+export default ExpandInfo
