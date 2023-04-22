0b9b0d873564004e3d3b92213e85a985ba642e8f
Hapnees <8dwardm8lton@gmail.com>
2023-04-22 13:04:04 +0300

    Feat (components): add ModalWindow

---
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -0,0 +1,22 @@
+import { DetailedHTMLProps, FC, HTMLAttributes } from 'react'
+import { IoCloseOutline } from 'react-icons/io5'
+import style from './ModalWindow.module.scss'
+
+interface IProps
+	extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
+	isShow?: boolean
+	closeFunc: () => void
+}
+
+const ModalWindow: FC<IProps> = ({ children, isShow, closeFunc, ...props }) => {
+	return (
+		<section
+			className={`${style.wrapper} ${!isShow ? style.hide : ''}`}
+			{...props}
+		>
+			<IoCloseOutline className={style.close_icon} onClick={closeFunc} />
+			{children}
+		</section>
+	)
+}
+export default ModalWindow
