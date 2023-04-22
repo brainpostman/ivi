8657ac0c5175ff9885fe96e2a7ef32eb697ea351
Marat Sattarov <sattarovmk@yandex.ru>
2023-04-22 20:21:20 +0500

    Feat (root) Next Auth setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,14 @@ import '@/styles/globals.scss';
 
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
+import { SessionProvider } from 'next-auth/react';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
         <Provider store={store}>
-            <Component {...pageProps} />
+            <SessionProvider session={session}>
+                <Component {...pageProps} />
+            </SessionProvider>
         </Provider>
     );
 }
