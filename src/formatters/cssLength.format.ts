98ee542ae3743a53cc1c58d632c5157323497f66
Hapnees <8dwardm8lton@gmail.com>
2023-04-24 00:09:35 +0300

    Feat (root): add formatters folder; add cssLength formatter

---
+++ src/formatters/cssLength.format.ts
@@ -0,0 +1,24 @@
+const formatCssLength = (length: string | number) => {
+  const stringedLen = length.toString()
+
+  if (length.toString().length < 2) {
+    console.error('UNCORRECT CSS LENGTH')
+    return `0px`
+  }
+
+  const lastElem = stringedLen.at(-1)
+  if (lastElem === '%') {
+    return length
+  }
+
+  const twoLastElems = stringedLen.at(-2)
+  if (twoLastElems === 'px') {
+    return length
+  }
+
+  if (+length) {
+    return `${length}px`
+  }
+}
+
+export default formatCssLength
