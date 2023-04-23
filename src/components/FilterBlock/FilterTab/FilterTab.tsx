bfb91b9137f9eb369720b002e4361e602211a359
Hapnees <8dwardm8lton@gmail.com>
2023-04-23 20:18:07 +0300

    Feat (FilterTab): refactor

--- src/components/FilterBlock/FilterTab/FilterTab.tsx
+++ src/components/FilterBlock/FilterTab/FilterTab.tsx
@@ -1,34 +1,44 @@
-import { IFilterListEl, IFilterTitle } from '@/types/filterBlock.interface'
+import { IFilterBlockEl, IFilterTitle } from '@/types/filterBlock.interface'
 import { DetailedHTMLProps, FC, HTMLAttributes } from 'react'
 import { MdArrowBackIosNew } from 'react-icons/md'
 import style from './FilterTab.module.scss'
 
 interface IProps
-	extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-	title: IFilterTitle
-	selectFilter: () => void
-	filter?: IFilterListEl
+  extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  title: IFilterTitle
+  selectFilter: () => void
+  filter?: IFilterBlockEl
+  paramValue?: string
 }
 
-const FilterTab: FC<IProps> = ({ title, selectFilter, filter, children }) => {
-	const titleClassName = `${style.title} ${
-		filter?.isExpand ? style.title__active : ''
-	}`
+const FilterTab: FC<IProps> = ({
+  title,
+  selectFilter,
+  filter,
+  paramValue,
+  children,
+}) => {
+  const titleClassName = `${style.title} ${
+    filter?.isExpand ? style.title__active : ''
+  }`
 
-	const arrowClassName = `${style.arrow} ${
-		filter?.isExpand ? style.arrow__active : ''
-	}`
+  const arrowClassName = `${style.arrow} ${
+    filter?.isExpand ? style.arrow__active : ''
+  }`
 
-	return (
-		<article className={style.wrapper}>
-			<div className={titleClassName} onClick={selectFilter}>
-				<h1>{title}</h1>
-				<MdArrowBackIosNew className={arrowClassName} />
-			</div>
+  return (
+    <article className={style.wrapper}>
+      <div className={titleClassName} onClick={selectFilter}>
+        <div>
+          <h1>{title}</h1>
+          {!!paramValue && <p className={style.param}>{paramValue}</p>}
+        </div>
+        <MdArrowBackIosNew className={arrowClassName} />
+      </div>
 
-			{children}
-		</article>
-	)
+      {children}
+    </article>
+  )
 }
 
 export default FilterTab
