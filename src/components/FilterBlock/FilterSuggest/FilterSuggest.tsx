71cba22ade66ea6c2fdc8d856c8f2bd369bcabe2
Hapnees <8dwardm8lton@gmail.com>
2023-04-23 20:18:35 +0300

    Feat (FilterSuggest): refactor; add hooks

--- src/components/FilterBlock/FilterSuggest/FilterSuggest.tsx
+++ src/components/FilterBlock/FilterSuggest/FilterSuggest.tsx
@@ -1,60 +1,94 @@
 import ModalWindow from '@/components/ModalWindow/ModalWindow'
 import Input from '@/components/UI/Input/Input'
-import { IFilterListEl, IFilterTitle } from '@/types/filterBlock.interface'
-import { ChangeEvent, FC, useMemo, useState } from 'react'
+import { useSetStringParam } from '@/hooks/useSetStringParam'
+import { IFilterBlockEl, IFilterTitle } from '@/types/filterBlock.interface'
+import { useRouter } from 'next/router'
+import {
+  ChangeEvent,
+  FC,
+  KeyboardEvent,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
 import FilterTab from '../FilterTab/FilterTab'
 import style from './FilterSuggest.module.scss'
 
 interface IProps {
-	title: IFilterTitle
-	filter?: IFilterListEl
-	selectFilter: () => void
-	closeModal: () => void
-	suggestList: string[]
-	placeholder?: string
+  title: IFilterTitle
+  filter?: IFilterBlockEl
+  selectFilter: () => void
+  closeModal: () => void
+  suggestList: string[]
+  placeholder?: string
+  query: string
 }
 
 const FilterSuggest: FC<IProps> = ({
-	title,
-	filter,
-	selectFilter,
-	closeModal,
-	suggestList: suggsetData,
-	placeholder,
+  title,
+  filter,
+  selectFilter,
+  closeModal,
+  suggestList: suggsetData,
+  placeholder,
+  query,
 }) => {
-	const [value, setValue] = useState('')
+  const router = useRouter()
+  const { param, setUrl, value: valueFromParams } = useSetStringParam(query)
+  const [value, setValue] = useState(valueFromParams)
 
-	const onChange = (event: ChangeEvent<HTMLInputElement>) => {
-		setValue(event.target.value)
-	}
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  }
 
-	const suggestList = useMemo(() => {
-		if (!value) return suggsetData.slice(0, 4)
+  const onKeyDownEnter = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      setUrl(value)
+      closeModal()
+    }
+  }
 
-		return suggsetData.filter(producer =>
-			producer.toLowerCase().includes(value.toLowerCase())
-		)
-	}, [value])
+  const suggestList = useMemo(() => {
+    if (!value) return suggsetData.slice(0, 4)
 
-	return (
-		<FilterTab title={title} selectFilter={selectFilter} filter={filter}>
-			<ModalWindow isShow={filter?.isExpand} closeFunc={closeModal}>
-				<div className={style.wrapper}>
-					<h1 className={style.title}>{title}</h1>
-					<Input
-						placeholder={placeholder}
-						value={value}
-						onChange={event => onChange(event)}
-					/>
-					<ul className={style.list}>
-						{suggestList.map(suggest => (
-							<li key={suggest}>{suggest}</li>
-						))}
-					</ul>
-				</div>
-			</ModalWindow>
-		</FilterTab>
-	)
+    return suggsetData.filter(producer =>
+      producer.toLowerCase().includes(value.toString().toLowerCase())
+    )
+  }, [value])
+
+  const onClickListEl = (_param: string) => () => {
+    setValue(_param)
+  }
+
+  return (
+    <FilterTab
+      title={title}
+      selectFilter={selectFilter}
+      filter={filter}
+      paramValue={param}
+    >
+      <ModalWindow isShow={filter?.isExpand} closeFunc={closeModal}>
+        <div className={style.wrapper}>
+          <h1 className={style.title}>{title}</h1>
+          <Input
+            type='text'
+            placeholder={placeholder}
+            value={value}
+            onChange={event => onChange(event)}
+            onKeyDown={event => onKeyDownEnter(event)}
+            autoFocus
+          />
+          <ul className={style.list}>
+            {suggestList.map(suggest => (
+              <li key={suggest} onClick={onClickListEl(suggest)}>
+                {suggest}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </ModalWindow>
+    </FilterTab>
+  )
 }
 
 export default FilterSuggest
