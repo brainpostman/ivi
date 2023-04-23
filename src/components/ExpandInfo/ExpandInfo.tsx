de28b0e872b3b90f6cca1649d82cbb540dd84c7d
Hapnees <8dwardm8lton@gmail.com>
2023-04-24 00:10:34 +0300

    Feat (ExpandInfo): add width prop

--- src/components/ExpandInfo/ExpandInfo.tsx
+++ src/components/ExpandInfo/ExpandInfo.tsx
@@ -1,44 +1,53 @@
+import formatCssLength from '@/formatters/cssLength.format'
 import { DetailedHTMLProps, FC, HTMLAttributes, useState } from 'react'
 import style from './ExpandInfo.module.scss'
 
 interface IProps
-	extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
-	title?: string
-	visibleText?: string
+  extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
+  title?: string
+  visibleText?: string
+  width?: string | number
 }
 
 const ExpandInfo: FC<IProps> = ({
-	children,
-	title,
-	visibleText,
-	className,
-	...props
+  children,
+  title,
+  visibleText,
+  className,
+  width = '75%',
+  ...props
 }) => {
-	const [isExpand, setIsExpand] = useState(false)
+  const [isExpand, setIsExpand] = useState(false)
 
-	const onClickToggle = () => setIsExpand(prev => !prev)
+  const onClickToggle = () => setIsExpand(prev => !prev)
 
-	return (
-		<article className={`${style.info} ${className}`} {...props}>
-			{title && <h1 className={style.info__title}>{title}</h1>}
+  const formattedWidth = formatCssLength(width)
 
-			{visibleText && (
-				<p
-					className={`${style.info__body__top} ${
-						isExpand ? style.visible : ''
-					}`}
-				>
-					{visibleText}
-				</p>
-			)}
+  return (
+    <article
+      className={`${style.info} ${className}`}
+      style={{ width: formattedWidth }}
+      {...props}
+    >
+      {title && <h1 className={style.info__title}>{title}</h1>}
 
-			{isExpand && children}
+      {visibleText && (
+        <p
+          className={`${style.info__body__top} ${
+            isExpand ? style.visible : ''
+          }`}
+        >
+          {visibleText}
+        </p>
+      )}
 
-			<p className={style.toggle} onClick={onClickToggle}>
-				{isExpand ? 'Свернуть' : 'Развернуть'}
-			</p>
-		</article>
-	)
+      {isExpand && children}
+
+      <p className={style.toggle} onClick={onClickToggle}>
+        {isExpand ? 'Свернуть' : 'Развернуть'}
+      </p>
+    </article>
+  )
 }
 
 export default ExpandInfo
