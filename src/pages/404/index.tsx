35346722d67d08a0b74099beef00708f715bd7bc
Hapnees <8dwardm8lton@gmail.com>
2023-04-24 00:26:24 +0300

    Feat (pages/404): refactor

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -3,10 +3,10 @@ import style from './index.module.scss'
 export default function Error() {
   return (
     <div className={style.wrapper}>
-        <div className={style.content}>
-        <h1>Ошибка</h1>
-        <h2>Запрашиваемой страницы не существует</h2>
-        </div>
+      <div className={style.content}>
+        <h1 className={style.title}>Ошибка</h1>
+        <h2 className={style.subtitle}>Запрашиваемой страницы не существует</h2>
+      </div>
     </div>
   )
 }
