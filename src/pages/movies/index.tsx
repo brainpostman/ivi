eb44fd5a30430657088bc14ce83b666c0c12548a
Hapnees <8dwardm8lton@gmail.com>
2023-04-23 20:21:07 +0300

    Feat (movies/index): small refactor

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -1,48 +1,58 @@
 import CustomCarousel from '@/components/CustomCarousel/CustomCarousel'
 import ExpandInfo from '@/components/ExpandInfo/ExpandInfo'
 import FilterBlock from '@/components/FilterBlock/FilterBlock'
+import MovieCardGrid from '@/components/MovieCardGrid/MovieCardGrid'
 import Sort from '@/components/Sort/Sort'
 import VioletButton from '@/components/UI/VioletButton/VioletButton'
+import { movieCarouselData } from '@/data/movieCarousel.data'
 import { moviesPageInfoData } from '@/data/moviesPage.data'
 import { yearCarouselData } from '@/data/yearCarousel.data'
 import PageLayout from '@/layouts/PageLayout'
+import { useRouter } from 'next/router'
 import style from './index.module.scss'
 
 const title =
-	'Смотреть фильмы онлайн бесплатно в хорошем HD качестве и без регистрации. Удобный просмотр онлайн фильмов на ivi.ru'
+  'Смотреть фильмы онлайн бесплатно в хорошем HD качестве и без регистрации. Удобный просмотр онлайн фильмов на ivi.ru'
 
 const visibleText = moviesPageInfoData[0]
 
 const MoviesPage = () => {
-	return (
-		<PageLayout title={title}>
-			{/*BREAD CRUMBS*/}
+  const router = useRouter()
 
-			<h1 className={style.title}>Фильмы смотреть онлайн</h1>
+  return (
+    <PageLayout title={title}>
+      {/*BREAD CRUMBS*/}
 
-			<ExpandInfo visibleText={visibleText}>
-				<div className={style.info}>
-					{moviesPageInfoData.slice(1).map((info, index) => (
-						<p key={index}>{info}</p>
-					))}
-				</div>
-			</ExpandInfo>
+      <h1 className={style.title}>Фильмы смотреть онлайн</h1>
 
-			<Sort />
+      <ExpandInfo visibleText={visibleText}>
+        <div className={style.info}>
+          {moviesPageInfoData.slice(1).map((info, index) => (
+            <p key={index}>{info}</p>
+          ))}
+        </div>
+      </ExpandInfo>
 
-			<CustomCarousel
-				children={VioletButton}
-				elementsView={7}
-				elementsMove={2}
-				data={yearCarouselData}
-				arrowSize={16}
-				classNameWrapper={style.carousel_wrapper}
-				space={12}
-			/>
+      <CustomCarousel
+        children={VioletButton}
+        elementsView={8}
+        elementsMove={2}
+        data={yearCarouselData}
+        arrowSize={16}
+        classNameWrapper={style.carousel_wrapper}
+        space={12}
+      />
 
-			<FilterBlock />
-		</PageLayout>
-	)
+      {!!Object.keys(router.query).length && <Sort />}
+
+      <FilterBlock />
+
+      <MovieCardGrid
+        movies={movieCarouselData}
+        className={style.moviegrid_wrapper}
+      />
+    </PageLayout>
+  )
 }
 
 export default MoviesPage
