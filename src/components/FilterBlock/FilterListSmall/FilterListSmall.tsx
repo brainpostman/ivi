0b113b658cef29eb429eea2629fc146951d6dae2
Hapnees <8dwardm8lton@gmail.com>
2023-04-23 20:18:57 +0300

    Feat (FilterListSmall): refactor; add hooks

--- src/components/FilterBlock/FilterListSmall/FilterListSmall.tsx
+++ src/components/FilterBlock/FilterListSmall/FilterListSmall.tsx
@@ -1,51 +1,61 @@
-import { IFilterListEl, IFilterTitle } from '@/types/filterBlock.interface'
+import { useSetListParam } from '@/hooks/useSetListParam'
+import {
+  IFilterBlockEl,
+  IFilterListEl,
+  IFilterTitle,
+} from '@/types/filterBlock.interface'
 import { FC, useState } from 'react'
 import FilterTab from '../FilterTab/FilterTab'
 import style from './FilterListSmall.module.scss'
 
 interface IProps {
-	title: IFilterTitle
-	selectFilter: () => void
-	filter?: IFilterListEl
-	list: string[]
+  title: IFilterTitle
+  selectFilter: () => void
+  filter?: IFilterBlockEl
+  list: Omit<IFilterListEl, 'isSelect'>[]
+  query: string
 }
 
-const FilterListSmall: FC<IProps> = ({ title, selectFilter, filter, list }) => {
-	const [items, setItem] = useState(
-		list.map((el, index) => ({ title: el, isSelect: !index }))
-	)
+const FilterListSmall: FC<IProps> = ({
+  title,
+  selectFilter,
+  filter,
+  list: listData,
+  query,
+}) => {
+  const { onClickListEl, param, list } = useSetListParam(
+    listData.map((el, index) => ({ ...el, isSelect: !index })),
+    query,
+    { filterType: 'radio', extraValues: ['all'] }
+  )
 
-	const selectItem = (title: string) => () => {
-		setItem(prev =>
-			prev.map(el => ({
-				...el,
-				isSelect: el.title === title ? !el.isSelect : false,
-			}))
-		)
-	}
-
-	return (
-		<FilterTab title={title} selectFilter={selectFilter} filter={filter}>
-			{filter?.isExpand && (
-				<div className={style.wrapper}>
-					<ul className={style.list}>
-						{items.map(item => (
-							<li key={item.title} onClick={selectItem(item.title)}>
-								<p className={item.isSelect ? style.title__active : ''}>
-									{item.title}
-								</p>
-								<div
-									className={`${style.circle} ${
-										item.isSelect && style.circle__active
-									}`}
-								></div>
-							</li>
-						))}
-					</ul>
-				</div>
-			)}
-		</FilterTab>
-	)
+  return (
+    <FilterTab
+      title={title}
+      selectFilter={selectFilter}
+      filter={filter}
+      paramValue={param}
+    >
+      {filter?.isExpand && (
+        <div className={style.wrapper}>
+          <ul className={style.list}>
+            {list.map(item => (
+              <li key={item.title} onClick={onClickListEl(item.param)}>
+                <p className={item.isSelect ? style.title__active : ''}>
+                  {item.title}
+                </p>
+                <div
+                  className={`${style.circle} ${
+                    item.isSelect && style.circle__active
+                  }`}
+                ></div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </FilterTab>
+  )
 }
 
 export default FilterListSmall
