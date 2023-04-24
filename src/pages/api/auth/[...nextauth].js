33ae2ad176497b6a73a59bdaa0ea0fdc50a922ea
Marat Sattarov <sattarovmk@yandex.ru>
2023-04-24 15:18:34 +0500

    Feat (header, auth) set up pass, google, vk login

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,89 @@
 import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/vk';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import VkProvider from 'next-auth/providers/vk';
+import GoogleProvider from 'next-auth/providers/google';
+import axios from 'axios';
+
 export const authOptions = {
-    // Configure one or more authentication providers
+    session: {
+        jwt: true,
+        maxAge: 86400,
+    },
     providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
+        CredentialsProvider({
+            id: 'login',
+            name: 'login',
+            async authorize(credentials) {
+                try {
+                    const response = await axios.post(process.env.LOGIN, {
+                        email: credentials.email,
+                        password: credentials.password,
+                    });
+
+                    if (response.status === 201 && response.data.token) {
+                        const user = {
+                            email: credentials.email,
+                            accessToken: response.data.token,
+                        };
+                        return user;
+                    } else {
+                        return null;
+                    }
+                } catch (err) {
+                    console.log(err.response.data);
+                    throw Error(err);
+                }
+            },
         }),
-        // ...add more providers here
+        CredentialsProvider({
+            id: 'register',
+            name: 'register',
+            async authorize(credentials) {
+                try {
+                    const response = await axios.post(process.env.REGISTRATION, {
+                        email: credentials.email,
+                        password: credentials.password,
+                    });
+
+                    if (response.status === 201 && response.data.token) {
+                        const user = {
+                            email: credentials.email,
+                            accessToken: response.data.token,
+                        };
+                        return user;
+                    } else {
+                        return null;
+                    }
+                } catch (err) {
+                    console.log(err.response.data);
+                    throw Error(err);
+                }
+            },
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: 'https://accounts.google.com/o/oauth2/auth',
+        }),
+        VkProvider({
+            clientId: process.env.VK_CLIENT_ID,
+            clientSecret: process.env.VK_CLIENT_SECRET,
+        }),
     ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.user = user;
+                token.accessToken = user.accessToken;
+            }
+            console.log(user);
+            return token;
+        },
+        async session({ session, token }) {
+            session.user = token.user;
+            session.accessToken = token.accessToken;
+            return session;
+        },
+    },
 };
 export default NextAuth(authOptions);
