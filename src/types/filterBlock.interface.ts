e335e8c358e5ae7bb0205103be51f0cc4410675f
Hapnees <8dwardm8lton@gmail.com>
2023-04-25 13:35:31 +0300

    Feat (types/filterBlock): add new interfaces for props

--- src/types/filterBlock.interface.ts
+++ src/types/filterBlock.interface.ts
@@ -1,3 +1,5 @@
+import { FC } from 'react'
+
 export type IFilterTitle =
   | 'Жанры'
   | 'Страны'
@@ -8,11 +10,47 @@ export type IFilterTitle =
 
 export interface IFilterBlockEl {
   title: IFilterTitle
-  isExpand?: boolean
+  isExpand: boolean
 }
 
 export interface IFilterListEl {
   title: string
   param: string
   isSelect: boolean
+}
+
+export interface IFilterData {
+  filter: IFilterBlockEl | undefined
+  selectFilter: () => void
+}
+
+export interface IFilterListBigProps {
+  filterData: IFilterData
+  carouselData: any[]
+  list: Omit<IFilterListEl, 'isSelect'>[]
+  carouselContent: FC<any>
+  carouselElementsView?: number
+  carouselElementsMove?: number
+  query: string
+}
+
+export interface IFilterListSmallProps {
+  filterData: IFilterData
+  list: Omit<IFilterListEl, 'isSelect'>[]
+  query: string
+}
+
+export interface IFilterSuggestProps {
+  filterData: IFilterData
+  closeModal: () => void
+  suggestList: string[]
+  placeholder?: string
+  query: string
+}
+
+export interface IFilterSliderProps {
+  title: string
+  query: string
+  minValue: number
+  maxValue: number
 }
