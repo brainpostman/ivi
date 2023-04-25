7bc4d12557804deb3e12960d48047d2d468c0918
Hapnees <8dwardm8lton@gmail.com>
2023-04-25 13:55:52 +0300

    Feat (MovieCard): move components

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,44 +1,44 @@
-import { IMovieCarouselElem } from '@/types/moviecarousel.interface'
+import { IMovieCard } from '@/types/movieCard.interface'
 import Image from 'next/image'
 import React, { FC, forwardRef } from 'react'
 import style from './MovieCard.module.scss'
-import MovieCarouselControls from './MovieCarouselControls/MovieCarouselControls'
-import MovieCarouselInfo from './MovieCarouselInfo/MovieCarouselInfo'
+import MovieCardControls from './MovieCardControls/MovieCardControls'
+import MovieCardInfo from './MovieCardInfo/MovieCardInfo'
 
 interface IProps {
-	movie: IMovieCarouselElem
+  movie: IMovieCard
 }
 
 const MovieCard = forwardRef<HTMLDivElement, IProps>(({ movie }, ref) => {
-	return (
-		<div key={movie.id} className={style.wrapper} ref={ref}>
-			<div className={style.wrapperImg}>
-				<Image
-					src={movie.img}
-					alt='movie'
-					className={style.img}
-					width={153}
-					height={235}
-				/>
-			</div>
-			<div className={style.hoverBlock}>
-				<MovieCarouselControls />
-				<MovieCarouselInfo block={movie} />
-			</div>
-			<div>
-				<p className={style.infoTitle}>{movie.title}</p>
-				<p
-					className={
-						movie.isFree
-							? style.infoSubtitle
-							: `${style.infoSubtitle} ${style.notFree}`
-					}
-				>
-					{movie.isFree ? 'Бесплатно' : 'Подписка'}
-				</p>
-			</div>
-		</div>
-	)
+  return (
+    <div key={movie.id} className={style.wrapper} ref={ref}>
+      <div className={style.wrapperImg}>
+        <Image
+          src={movie.img}
+          alt='movie'
+          className={style.img}
+          width={153}
+          height={235}
+        />
+      </div>
+      <div className={style.hoverBlock}>
+        <MovieCardControls />
+        <MovieCardInfo block={movie} />
+      </div>
+      <div>
+        <p className={style.infoTitle}>{movie.title}</p>
+        <p
+          className={
+            movie.isFree
+              ? style.infoSubtitle
+              : `${style.infoSubtitle} ${style.notFree}`
+          }
+        >
+          {movie.isFree ? 'Бесплатно' : 'Подписка'}
+        </p>
+      </div>
+    </div>
+  )
 })
 
 MovieCard.displayName = 'MovieCard'
