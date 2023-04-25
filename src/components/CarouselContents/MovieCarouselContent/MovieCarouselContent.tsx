6f3108005f18628ac842a352d7c6a2686e5774d6
Hapnees <8dwardm8lton@gmail.com>
2023-04-25 13:56:19 +0300

    Fix (root): wrong whitespaces

--- src/components/CarouselContents/MovieCarouselContent/MovieCarouselContent.tsx
+++ src/components/CarouselContents/MovieCarouselContent/MovieCarouselContent.tsx
@@ -1,12 +1,12 @@
 import MovieCard from '@/components/MovieCard/MovieCard'
 import { useCustomCarouselContent } from '@/hooks/useCustomCarouselContent'
-import { IMovieCarouselElem } from '@/types/moviecarousel.interface'
+import { IMovieCard } from '@/types/moviecarousel.interface'
 import { FC } from 'react'
 
-const MovieCarouselContent: FC<IMovieCarouselElem> = (movie, addElementLen) => {
-	const wrapperRef = useCustomCarouselContent(addElementLen)
+const MovieCarouselContent: FC<IMovieCard> = (movie, addElementLen) => {
+  const wrapperRef = useCustomCarouselContent(addElementLen)
 
-	return <MovieCard movie={movie} ref={wrapperRef} />
+  return <MovieCard movie={movie} ref={wrapperRef} />
 }
 
 export default MovieCarouselContent
--- src/components/MovieCardGrid/MovieCardGrid.tsx
+++ src/components/MovieCardGrid/MovieCardGrid.tsx
@@ -1,26 +1,26 @@
-import { IMovieCarouselElem } from '@/types/moviecarousel.interface'
+import { IMovieCard } from '@/types/moviecarousel.interface'
 import { DetailedHTMLProps, FC, HTMLAttributes } from 'react'
 import MovieCard from '../MovieCard/MovieCard'
 import style from './MovieCardGrid.module.scss'
 
 interface IProps
-	extends DetailedHTMLProps<
-		HTMLAttributes<HTMLUListElement>,
-		HTMLUListElement
-	> {
-	movies: IMovieCarouselElem[]
+  extends DetailedHTMLProps<
+    HTMLAttributes<HTMLUListElement>,
+    HTMLUListElement
+  > {
+  movies: IMovieCard[]
 }
 
 const MovieCardGrid: FC<IProps> = ({ movies, className, ...props }) => {
-	return (
-		<ul className={`${style.wrapper} ${className}`} {...props}>
-			{movies.map(movie => (
-				<li key={movie.id}>
-					<MovieCard movie={movie} />
-				</li>
-			))}
-		</ul>
-	)
+  return (
+    <ul className={`${style.wrapper} ${className}`} {...props}>
+      {movies.map(movie => (
+        <li key={movie.id}>
+          <MovieCard movie={movie} />
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default MovieCardGrid
