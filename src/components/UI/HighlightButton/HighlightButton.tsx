cd35e809d041444191c77ca66bbb487efef67a9e
Marat Sattarov <sattarovmk@yandex.ru>
2023-04-25 19:19:16 +0500

    Feat (HighlightBtn) added :disabled style

--- src/components/UI/HighlightButton/HighlightButton.tsx
+++ src/components/UI/HighlightButton/HighlightButton.tsx
@@ -1,14 +1,14 @@
-import { ButtonHTMLAttributes, DetailedHTMLProps, FC } from 'react'
-import style from './HighlightButton.module.scss'
+import { ButtonHTMLAttributes, DetailedHTMLProps, FC } from 'react';
+import style from './HighlightButton.module.scss';
 
 const HighlightButton: FC<
-	DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+    DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
 > = ({ children, ...props }) => {
-	return (
-		<button className={style.button} {...props}>
-			{children}
-		</button>
-	)
-}
+    return (
+        <button className={style.button} {...props}>
+            {children}
+        </button>
+    );
+};
 
-export default HighlightButton
+export default HighlightButton;
