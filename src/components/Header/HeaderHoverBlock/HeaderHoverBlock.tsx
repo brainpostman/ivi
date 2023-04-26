1348381179e9651d4d3fb594812dde9dbe66e09f
Hapnees <8dwardm8lton@gmail.com>
2023-04-26 21:27:39 +0300

    Feat (Header): change HeaderCarousel to CustomCarousel

--- src/components/Header/HeaderHoverBlock/HeaderHoverBlock.tsx
+++ src/components/Header/HeaderHoverBlock/HeaderHoverBlock.tsx
@@ -1,6 +1,6 @@
 import { headerHoverBlockContent } from '@/data/headerHoverBlock.data'
 import { IHeaderTab } from '@/types/header.interface'
-import { FC } from 'react'
+import { DetailedHTMLProps, FC, HTMLAttributes } from 'react'
 import style from './HeaderHoverBlock.module.scss'
 import HeaderProfileBlock from './HeaderProfileBlock/HeaderProfileBlock'
 import HeaderTvBlock from './HeaderTvBlock/HeaderTvBlock'
@@ -8,12 +8,12 @@ import HeaderWatchBlock from './HeaderWatchBlock/HeaderWatchBlock'
 import HeaderMovieBlock from './HedaerMovieBlock/HeaderMovieBlock'
 import HoverTabBlock from './HoverFilterBlock/HoverTabBlock'
 
-interface IProps {
+interface IProps
+  extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
   tab: IHeaderTab
-  hideHoverBlock: () => void
 }
 
-const HeaderHoverBlock: FC<IProps> = ({ hideHoverBlock, tab }) => {
+const HeaderHoverBlock: FC<IProps> = ({ tab, ...props }) => {
   const tabList: IHeaderTab[] = ['movies', 'series', 'cartoons']
 
   const tabsWithMovieBlock: IHeaderTab[] = [
@@ -26,7 +26,7 @@ const HeaderHoverBlock: FC<IProps> = ({ hideHoverBlock, tab }) => {
   const currentBlock = headerHoverBlockContent.find(block => block.tab === tab)
 
   return (
-    <article className={style.wrapper} onMouseLeave={hideHoverBlock}>
+    <article className={style.wrapper} {...props}>
       {tabList.includes(tab) ? (
         <HoverTabBlock currentBlock={currentBlock} />
       ) : tab === 'TV+' ? (
