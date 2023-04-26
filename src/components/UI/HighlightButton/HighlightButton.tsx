bc54e1cf691edcb65237dc98f8c2839c0866fbcc
Hapnees <8dwardm8lton@gmail.com>
2023-04-26 21:29:20 +0300

    Feat (HighlightButton): refactor

--- src/components/UI/HighlightButton/HighlightButton.tsx
+++ src/components/UI/HighlightButton/HighlightButton.tsx
@@ -2,13 +2,13 @@ import { ButtonHTMLAttributes, DetailedHTMLProps, FC } from 'react'
 import style from './HighlightButton.module.scss'
 
 const HighlightButton: FC<
-	DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
-> = ({ children, ...props }) => {
-	return (
-		<button className={style.button} {...props}>
-			{children}
-		</button>
-	)
+  DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>
+> = ({ children, className, ...props }) => {
+  return (
+    <button className={`${style.button} ${className}`} {...props}>
+      {children}
+    </button>
+  )
 }
 
 export default HighlightButton
