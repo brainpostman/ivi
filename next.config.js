2a44b9613c145e60e62a66c80d0e482371513966
Marat Sattarov <sattarovmk@yandex.ru>
2023-04-28 19:16:21 +0500

    Feat (root) i18next install and configuration

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
+import { i18n } from './next-i18next.config.js';
 
 const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    additionalData: `@import "./src/styles/vars.scss"; @import "./src/styles/index.scss"; @import "./src/styles/ui-kit.scss";`,
-  },
-  images: {
-    domains: ['thumbs.dfs.ivi.ru'],
-  },
-}
+    reactStrictMode: true,
+    sassOptions: {
+        additionalData: `@import "./src/styles/vars.scss"; @import "./src/styles/index.scss"; @import "./src/styles/ui-kit.scss";`,
+    },
+    images: {
+        domains: ['thumbs.dfs.ivi.ru'],
+    },
+    i18n,
+};
 
-module.exports = nextConfig
+export default nextConfig;
