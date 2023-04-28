5dac12f19ea42eaf156887798f7920bfc58d810d
Marat Sattarov <sattarovmk@yandex.ru>
2023-04-29 04:29:45 +0500

    Feat (AuthModal) authorization en localization

--- src/utils/auth.util.ts
+++ src/utils/auth.util.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import { i18n } from 'next-i18next';
 
 type CheckEmailResponse = {
     status: number;
 };
+
+i18n?.loadNamespaces(['auth_modal']);
 
 export async function checkEmailVacancy(email: string): Promise<string> {
     try {
@@ -23,7 +26,7 @@ export async function checkEmailVacancy(email: string): Promise<string> {
             return err.message;
         } else {
             console.error('Error: ', err);
-            return 'Произошла непредвиденная ошибка';
+            return i18n?.t('error-messages.unforeseen-error') ?? 'Произошла непредвиденная ошибка';
         }
     }
 }
@@ -34,23 +37,28 @@ export function validateEmail(email: string): string {
     if (emailRegex.test(email)) {
         return '';
     } else {
-        return 'Некорректная электронная почта';
+        return (
+            i18n?.t('auth_modal:error-messages.incorrect-email') ?? 'Некорректная электронная почта'
+        );
     }
 }
 
 export function validatePassword(password: string): string[] {
     let messages: string[] = [];
     if (password.length === 0) {
-        messages.push('Пожалуйста, введите пароль');
+        messages.push(i18n?.t('error-messages.min-length') ?? 'Пожалуйста, введите пароль');
         return messages;
     }
     if (password.length < 6) {
-        messages.push('Пароль должен иметь как минимум 6 символов');
+        messages.push(
+            i18n?.t('error-messages.min-length') ?? 'Пароль должен иметь как минимум 6 символов'
+        );
     }
     const passwordRegex = /^[a-zA-Z0-9!@#$%^&*()_+-=,.<>?;:'"\\/[\]{}|`~]+$/;
     if (!passwordRegex.test(password)) {
         messages.push(
-            'Пароль должен содержать только символы латинского алфавита, цифры и спецсимволы'
+            i18n?.t('error-messages.no-pattern-match') ??
+                'Пароль должен содержать только символы латинского алфавита, цифры и спецсимволы'
         );
     }
     return messages;
@@ -60,6 +68,6 @@ export function validateConfirmedPassword(password: string, confirmation: string
     if (password === confirmation) {
         return '';
     } else {
-        return 'Пароли должны совпадать';
+        return i18n?.t('error-messages.no-confirmation-match') ?? 'Пароли должны совпадать';
     }
 }
