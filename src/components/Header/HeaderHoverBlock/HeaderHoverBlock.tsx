e3a033bb5ce41f091e7e57bc70577dfc1b88f70d
Marat Sattarov <sattarovmk@yandex.ru>
2023-04-28 20:36:48 +0500

    Feat (pages/locales) fixed json structure

--- src/components/Header/HeaderHoverBlock/HeaderHoverBlock.tsx
+++ src/components/Header/HeaderHoverBlock/HeaderHoverBlock.tsx
@@ -1,47 +1,41 @@
-import { headerHoverBlockContent } from '@/data/headerHoverBlock.data'
-import { IHeaderTab } from '@/types/header.interface'
-import { DetailedHTMLProps, FC, HTMLAttributes } from 'react'
-import style from './HeaderHoverBlock.module.scss'
-import HeaderProfileBlock from './HeaderProfileBlock/HeaderProfileBlock'
-import HeaderTvBlock from './HeaderTvBlock/HeaderTvBlock'
-import HeaderWatchBlock from './HeaderWatchBlock/HeaderWatchBlock'
-import HeaderMovieBlock from './HedaerMovieBlock/HeaderMovieBlock'
-import HoverTabBlock from './HoverFilterBlock/HoverTabBlock'
+import { headerHoverBlockContent } from '@/data/headerHoverBlock.data';
+import { IHeaderTab } from '@/types/header.interface';
+import { DetailedHTMLProps, FC, HTMLAttributes } from 'react';
+import style from './HeaderHoverBlock.module.scss';
+import HeaderProfileBlock from './HeaderProfileBlock/HeaderProfileBlock';
+import HeaderTvBlock from './HeaderTvBlock/HeaderTvBlock';
+import HeaderWatchBlock from './HeaderWatchBlock/HeaderWatchBlock';
+import HeaderMovieBlock from './HedaerMovieBlock/HeaderMovieBlock';
+import HoverTabBlock from './HoverFilterBlock/HoverTabBlock';
 
-interface IProps
-  extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
-  tab: IHeaderTab
-  hideHoverBlock: () => void
+interface IProps extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
+    tab: IHeaderTab;
+    hideHoverBlock: () => void;
 }
 
 const HeaderHoverBlock: FC<IProps> = ({ tab, hideHoverBlock, ...props }) => {
-  const tabList: IHeaderTab[] = ['movies', 'series', 'cartoons']
+    const tabList: IHeaderTab[] = ['movies', 'series', 'cartoons'];
 
-  const tabsWithMovieBlock: IHeaderTab[] = [
-    'movies',
-    'series',
-    'cartoons',
-    'TV+',
-  ]
+    const tabsWithMovieBlock: IHeaderTab[] = ['movies', 'series', 'cartoons', 'TV+'];
 
-  const currentBlock = headerHoverBlockContent.find(block => block.tab === tab)
+    const currentBlock = headerHoverBlockContent.find((block) => block.tab === tab);
 
-  return (
-    <article className={style.wrapper} {...props}>
-      {tabList.includes(tab) ? (
-        <HoverTabBlock currentBlock={currentBlock} />
-      ) : tab === 'TV+' ? (
-        <HeaderTvBlock />
-      ) : tab === 'profile' ? (
-        <HeaderProfileBlock />
-      ) : tab === 'watch' ? (
-        <HeaderWatchBlock />
-      ) : (
-        <></>
-      )}
-      {tabsWithMovieBlock.includes(tab) && <HeaderMovieBlock />}
-    </article>
-  )
-}
+    return (
+        <article className={style.wrapper} {...props}>
+            {tabList.includes(tab) ? (
+                <HoverTabBlock currentBlock={currentBlock} />
+            ) : tab === 'TV+' ? (
+                <HeaderTvBlock />
+            ) : tab === 'profile' ? (
+                <HeaderProfileBlock />
+            ) : tab === 'watch' ? (
+                <HeaderWatchBlock />
+            ) : (
+                <></>
+            )}
+            {tabsWithMovieBlock.includes(tab) && <HeaderMovieBlock />}
+        </article>
+    );
+};
 
-export default HeaderHoverBlock
+export default HeaderHoverBlock;
