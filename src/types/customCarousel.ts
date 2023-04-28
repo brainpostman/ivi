128616cb73c5577271a55fa8217b114104f5adab
Hapnees <8dwardm8lton@gmail.com>
2023-04-28 16:37:32 +0300

    Feat (types): add customCarousel types

---
+++ src/types/customCarousel.ts
@@ -0,0 +1,20 @@
+import { Url } from 'next/dist/shared/lib/router/router'
+
+export type ICustomCarouselWidth = 'full' | 'fit' | 'fit-shadow'
+
+export interface ICustomCarouselProps {
+  elementsView: number
+  elementsMove: number
+  title?: string
+  href?: Url
+  children: React.ReactNode[]
+  additElem?: () => JSX.Element
+  classNameList?: string
+  classNameWrapper?: string
+  arrowSize?: number
+  space?: number[]
+  speed?: number
+  width?: ICustomCarouselWidth
+  breakpoints?: number[]
+  padding?: number
+}
