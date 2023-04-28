b990c0b8e3cca640c58aaa2a283e85a5735cc0c2
Marat Sattarov <sattarovmk@yandex.ru>
2023-04-28 20:22:11 +0500

    Fix (root) fixed next config export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { i18n } = require('./next-i18next.config.js');
 
 const nextConfig = {
@@ -12,4 +13,4 @@ const nextConfig = {
     i18n,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
