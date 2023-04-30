1695b97c928460c6fef4050c5b7a8688dadbcb69
Hapnees <8dwardm8lton@gmail.com>
2023-04-30 12:45:49 +0300

    Feat (BasicBtn): add PROPS comments; add red circle

--- src/components/UI/BasicBtn/BasicBtn.interface.ts
+++ src/components/UI/BasicBtn/BasicBtn.interface.ts
@@ -1,18 +1,29 @@
-import { IBtnType } from '@/types/button.interface';
-import { HTMLAttributeAnchorTarget } from 'react';
+import { IBtnType } from '@/types/button.interface'
+import { HTMLAttributeAnchorTarget } from 'react'
 
-// @href - работает как обычная ссылка
-// @navlink - работает как тэг Link
+// ==== PROPS ====
+// @param btnType - тип внешнего вида кнопки
+// @param href - обычная ссылка
+// @param navlink - ссылка для навигации
+// Испольузем либо href, либо navlink
+// @param suptitle - текст над текстом кнопки
+// Работает при btnType = 'textPlusIcon'
+// @param title - текст кнопки
+// Работает при btnType = 'textPlusIcon'
+// @param target - поведение при нажатии на ссылку
+// Работает, если указан href
+// @param circle - показать красный кружок
 
 export interface IBasicBtnProps
-    extends React.DetailedHTMLProps<
-        React.ButtonHTMLAttributes<HTMLButtonElement>,
-        HTMLButtonElement
-    > {
-    btnType?: IBtnType;
-    href?: string;
-    navlink?: string;
-    subtitle?: string;
-    title?: string;
-    target?: HTMLAttributeAnchorTarget;
+  extends React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  btnType?: IBtnType
+  href?: string
+  navlink?: string
+  suptitle?: string
+  title?: string
+  target?: HTMLAttributeAnchorTarget
+  circle?: boolean
 }
