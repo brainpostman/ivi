4377312499c985880517ec9cab044c1819286b2b
Marat Sattarov <sattarovmk@yandex.ru>
2023-04-30 15:36:07 +0500

    Fix (LanguageChanger) different reload method

--- src/components/LanguageChanger/LanguageChanger.tsx
+++ src/components/LanguageChanger/LanguageChanger.tsx
@@ -1,33 +1,33 @@
-import Router, { useRouter } from 'next/router';
-import Switch from '../UI/Switch/Switch';
-import { delay } from '@/utils/delay';
-import setLocaleCookie from '@/utils/localization.utils';
+import { delay } from '@/utils/delay'
+import setLocaleCookie from '@/utils/localization.utils'
+import Router, { useRouter } from 'next/router'
+import Switch from '../UI/Switch/Switch'
 
-const switchSpeed = 200;
+const switchSpeed = 200
 
 const LanguageChanger = () => {
-    const router = useRouter();
-    const { pathname, asPath, query } = router;
-    const onToggleLanguageClick = async (newLocale: string) => {
-        setLocaleCookie(newLocale);
-        await router.push({ pathname, query }, asPath, { locale: newLocale });
-        await delay(switchSpeed * 1.25);
-        Router.reload();
-    };
-    return (
-        <Switch
-            left={'РУ'}
-            right={'EN'}
-            startLeft={router.locale === 'ru'}
-            callbackLeft={() => {
-                onToggleLanguageClick('ru');
-            }}
-            callbackRight={() => {
-                onToggleLanguageClick('en');
-            }}
-            speedms={switchSpeed}
-        />
-    );
-};
+  const router = useRouter()
+  const { pathname, asPath, query } = router
+  const onToggleLanguageClick = async (newLocale: string) => {
+    setLocaleCookie(newLocale)
+    await router.push({ pathname, query }, asPath, { locale: newLocale })
+    await delay(switchSpeed * 1.25)
+    Router.reload()
+  }
+  return (
+    <Switch
+      left={'РУ'}
+      right={'EN'}
+      startLeft={router.locale === 'ru'}
+      callbackLeft={() => {
+        onToggleLanguageClick('ru')
+      }}
+      callbackRight={() => {
+        onToggleLanguageClick('en')
+      }}
+      speedms={switchSpeed}
+    />
+  )
+}
 
-export default LanguageChanger;
+export default LanguageChanger
