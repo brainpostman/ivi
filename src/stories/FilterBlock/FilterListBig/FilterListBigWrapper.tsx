854683bbec3ccbe7f17eeaf0579166282d7c18aa
Hapnees <8dwardm8lton@gmail.com>
2023-04-30 13:03:16 +0300

    Fix (stories/FilterBlock): carousels dont show

--- src/stories/FilterBlock/FilterListBig/FilterListBigWrapper.tsx
+++ src/stories/FilterBlock/FilterListBig/FilterListBigWrapper.tsx
@@ -1,5 +1,5 @@
-import FilterGenreCarouselContent from '@/components/CarouselContents/FilterGenreCarouselContent/FilterGenreCarouselContent'
 import FilterListBig from '@/components/FilterBlock/FilterListBig/FilterListBig'
+import FilterGenreCard from '@/components/FilterGenreCard/FilterGenreCard'
 import { useFilter } from '@/hooks/useFilter'
 import {
   IFilterBlockEl,
@@ -16,12 +16,17 @@ const filterList: Omit<IFilterBlockEl, 'isExpand'>[] = [
 const FilterListBigWrapper: FC<
   Omit<IFilterListBigProps, 'filterData'> & {
     title: IFilterTitle
+    children: React.ReactNode[]
   }
-> = ({ title, ...props }) => {
+> = ({ title, children, ...props }) => {
   const { getFilterData } = useFilter(filterList)
   const filterData = getFilterData(title)
 
-  return <FilterListBig filterData={filterData} {...props} />
+  return (
+    <FilterListBig filterData={filterData} {...props}>
+      {children}
+    </FilterListBig>
+  )
 }
 
 export default FilterListBigWrapper
