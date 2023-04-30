2be1799de018cb4efbf1fd21bce341bbf51d8e75
Hapnees <8dwardm8lton@gmail.com>
2023-04-30 13:03:48 +0300

    Feat (stories/BasicBtn): add prop 'circle'

--- src/stories/UI/BasicBtn/BasicBtn.stories.tsx
+++ src/stories/UI/BasicBtn/BasicBtn.stories.tsx
@@ -3,170 +3,175 @@ import { Meta } from '@storybook/react'
 import React from 'react'
 import { BiDevices } from 'react-icons/bi'
 import {
-	FaApple,
-	FaGooglePlay,
-	FaLinkedinIn,
-	FaOdnoklassniki,
-	FaTelegramPlane,
-	FaTv,
-	FaTwitter,
-	FaViber,
-	FaVk,
+  FaApple,
+  FaGooglePlay,
+  FaLinkedinIn,
+  FaOdnoklassniki,
+  FaTelegramPlane,
+  FaTv,
+  FaTwitter,
+  FaViber,
+  FaVk,
 } from 'react-icons/fa'
 import { FiPhone } from 'react-icons/fi'
 import { GoMail } from 'react-icons/go'
 
 const iconsforTypeIconCircle = {
-	empty: <></>,
-	linkedin: <FaLinkedinIn />,
-	OK: <FaOdnoklassniki />,
-	Telegram: <FaTelegramPlane />,
-	Twitter: <FaTwitter />,
-	Viber: <FaViber />,
-	VK: <FaVk />,
+  empty: <></>,
+  linkedin: <FaLinkedinIn />,
+  OK: <FaOdnoklassniki />,
+  Telegram: <FaTelegramPlane />,
+  Twitter: <FaTwitter />,
+  Viber: <FaViber />,
+  VK: <FaVk />,
 }
 
 const iconsForTypeTextPlusIcon = {
-	empty: <></>,
-	Apple: <FaApple />,
-	GooglePlay: <FaGooglePlay />,
-	TV: <FaTv />,
-	'Все устройства': <BiDevices />,
+  empty: <></>,
+  Apple: <FaApple />,
+  GooglePlay: <FaGooglePlay />,
+  TV: <FaTv />,
+  'Все устройства': <BiDevices />,
 }
 
 const iconsForTypeIcon = {
-	email: <GoMail />,
-	phone: <FiPhone />,
+  email: <GoMail />,
+  phone: <FiPhone />,
 }
 
 const meta: Meta = {
-	title: 'UI/BasicBtn',
-	parameters: {
-		docs: {
-			description: {
-				component: 'Самая часто используемая кнопка сайта',
-			},
-		},
-		backgrounds: { default: 'darkSoft' },
-	},
-	component: BasicBtn,
-	argTypes: {
-		children: {
-			name: 'label',
-			description: 'Текст кнопки',
-			if: {
-				arg: 'btnType',
-				eq: 'text',
-			},
-		},
+  title: 'UI/BasicBtn',
+  parameters: {
+    docs: {
+      description: {
+        component: 'Самая часто используемая кнопка сайта',
+      },
+    },
+    backgrounds: { default: 'darkSoft' },
+  },
+  component: BasicBtn,
+  argTypes: {
+    children: {
+      name: 'label',
+      description: 'Текст кнопки',
+      if: {
+        arg: 'btnType',
+        eq: 'text',
+      },
+    },
 
-		href: {
-			name: 'url',
-			description: 'Ссылка',
-		},
+    href: {
+      name: 'url',
+      description: 'Ссылка',
+    },
 
-		navlink: {
-			table: {
-				disable: true,
-			},
-		},
+    navlink: {
+      table: {
+        disable: true,
+      },
+    },
 
-		btnType: {
-			name: 'variant',
-			description: 'Вариант внешнего вида',
-			control: 'select',
-		},
+    btnType: {
+      name: 'variant',
+      description: 'Вариант внешнего вида',
+      control: 'select',
+    },
 
-		target: {
-			description: 'Поведение при клике',
-			options: ['_blank', '_parent', '_self', '_top'],
-			if: {
-				arg: 'href',
-				truthy: true,
-			},
-		},
+    target: {
+      description: 'Поведение при клике',
+      options: ['_blank', '_parent', '_self', '_top'],
+      if: {
+        arg: 'href',
+        truthy: true,
+      },
+    },
 
-		title: {
-			description: 'Главный текст',
-			defaultValue: '',
-			if: {
-				arg: 'btnType',
-				eq: 'textPlusIcon',
-			},
-		},
+    title: {
+      description: 'Главный текст',
+      defaultValue: '',
+      if: {
+        arg: 'btnType',
+        eq: 'textPlusIcon',
+      },
+    },
 
-		suptitle: {
-			description: 'Надзголовок',
-			defaultValue: '',
-			if: {
-				arg: 'btnType',
-				eq: 'textPlusIcon',
-			},
-		},
+    suptitle: {
+      description: 'Надзголовок',
+      defaultValue: '',
+      if: {
+        arg: 'btnType',
+        eq: 'textPlusIcon',
+      },
+    },
 
-		iconsforTypeIcon: {
-			name: 'icon',
-			description: 'Иконка',
-			options: Object.keys(iconsforTypeIconCircle),
-			mapping: iconsforTypeIconCircle,
-			control: 'select',
-			if: {
-				arg: 'btnType',
-				eq: 'iconCircle',
-			},
-		},
+    iconsforTypeIcon: {
+      name: 'icon',
+      description: 'Иконка',
+      options: Object.keys(iconsforTypeIconCircle),
+      mapping: iconsforTypeIconCircle,
+      control: 'select',
+      if: {
+        arg: 'btnType',
+        eq: 'iconCircle',
+      },
+    },
 
-		iconsForTypeTextPlusIcon: {
-			name: 'icon',
-			description: 'Иконка',
-			options: Object.keys(iconsForTypeTextPlusIcon),
-			mapping: iconsForTypeTextPlusIcon,
-			control: 'select',
-			if: {
-				arg: 'btnType',
-				eq: 'textPlusIcon',
-			},
-		},
+    iconsForTypeTextPlusIcon: {
+      name: 'icon',
+      description: 'Иконка',
+      options: Object.keys(iconsForTypeTextPlusIcon),
+      mapping: iconsForTypeTextPlusIcon,
+      control: 'select',
+      if: {
+        arg: 'btnType',
+        eq: 'textPlusIcon',
+      },
+    },
 
-		iconsForTypeIcon: {
-			name: 'icon',
-			description: 'Иконка',
-			options: Object.keys(iconsForTypeIcon),
-			mapping: iconsForTypeIcon,
-			control: 'select',
-			if: {
-				arg: 'btnType',
-				eq: 'icon',
-			},
-		},
-	},
+    iconsForTypeIcon: {
+      name: 'icon',
+      description: 'Иконка',
+      options: Object.keys(iconsForTypeIcon),
+      mapping: iconsForTypeIcon,
+      control: 'select',
+      if: {
+        arg: 'btnType',
+        eq: 'icon',
+      },
+    },
+
+    circle: {
+      description: 'Красный кружок',
+    },
+  },
 }
 
 export const Primary = ({
-	children,
-	iconsforTypeIcon,
-	iconsForTypeTextPlusIcon,
-	iconsForTypeIcon,
-	...props
+  children,
+  iconsforTypeIcon,
+  iconsForTypeTextPlusIcon,
+  iconsForTypeIcon,
+  ...props
 }: {
-	children: string
-	iconsforTypeIcon?: React.ReactElement
-	iconsForTypeTextPlusIcon?: React.ReactElement
-	iconsForTypeIcon?: React.ReactElement
+  children: string
+  iconsforTypeIcon?: React.ReactElement
+  iconsForTypeTextPlusIcon?: React.ReactElement
+  iconsForTypeIcon?: React.ReactElement
 } & typeof BasicBtn) => (
-	<BasicBtn {...props}>
-		{iconsForTypeIcon || iconsForTypeTextPlusIcon || iconsforTypeIcon}
-		{children}
-	</BasicBtn>
+  <BasicBtn {...props}>
+    {iconsForTypeIcon || iconsForTypeTextPlusIcon || iconsforTypeIcon}
+    {children}
+  </BasicBtn>
 )
 
 Primary.args = {
-	children: 'Написать в чате',
-	btnType: 'text',
-	href: '',
-	target: '_blank',
-	title: '',
-	suptitle: '',
+  children: 'Написать в чате',
+  btnType: 'text',
+  href: '',
+  target: '_blank',
+  title: '',
+  suptitle: '',
+  circle: false,
 }
 
 export default meta
