92d6bfcb06be5fdc6b22d6a30aecda844679f291
Hapnees <8dwardm8lton@gmail.com>
2023-05-01 14:29:34 +0300

    Feat (next.config): add domain

--- next.config.js
+++ next.config.js
@@ -1,33 +1,20 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { i18n } = require('./next-i18next.config.js');
+const { i18n } = require('./next-i18next.config.js')
 
 const nextConfig = {
-    reactStrictMode: true,
-    sassOptions: {
-        additionalData: `@import "./src/styles/vars.scss"; @import "./src/styles/index.scss"; @import "./src/styles/ui-kit.scss"; @import "./src/styles/adaptive.scss"; `,
-    },
-    images: {
-        domains: ['thumbs.dfs.ivi.ru'],
-        remotePatterns:[
-            {
-                protocol:'https',
-                hostname:'avatars.mds.yandex.net',
-                port:'',
-            },
-            {
-                protocol:'https',
-                hostname:'www.images.ru',
-                port:'',
-            },
-            {
-                protocol:'https',
-                hostname:'www.ivi.ru',
-                port:'',
-            }
-        ]
-    },
-    i18n,
-};
-
-module.exports = nextConfig;
+  reactStrictMode: true,
+  sassOptions: {
+    additionalData: `@import "./src/styles/vars.scss"; @import "./src/styles/index.scss"; @import "./src/styles/ui-kit.scss"; @import "./src/styles/adaptive.scss"; `,
+  },
+  images: {
+    domains: [
+      'thumbs.dfs.ivi.ru',
+      'avatars.mds.yandex.net',
+      'images.ru',
+      'ivi.ru',
+    ],
+  },
+  i18n,
+}
+module.exports = nextConfig
