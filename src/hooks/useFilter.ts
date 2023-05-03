2680c442a5eacef528bfd064afb11d1fbbf60353
Hapnees <8dwardm8lton@gmail.com>
2023-05-03 13:51:13 +0300

    Feat (hooks): rework useFilter & useSetListParam

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -1,30 +1,36 @@
-import { getFilterFuncs } from '@/components/FilterBlock/utils/filterBlock.util';
-import { IFilterBlockEl, IFilterData, IFilterTitle } from '@/types/filterBlock.interface';
-import { useState } from 'react';
+import { getFilterFuncs } from '@/utils/filterBlock.util'
+import {
+  IFilterBlockEl,
+  IFilterData,
+  IFilterTitle,
+} from '@/types/filterBlock.interface'
+import { useState } from 'react'
 
 export const useFilter = (defaultValue: Omit<IFilterBlockEl, 'isExpand'>[]) => {
-    const [filters, setFilters] = useState(defaultValue.map((el) => ({ ...el, isExpand: false })));
-    const titles = defaultValue.map((el) => el.title);
+  const [filters, setFilters] = useState(
+    defaultValue.map(el => ({ ...el, isExpand: false }))
+  )
+  const titles = defaultValue.map(el => el.title)
 
-    const filterFuncs = getFilterFuncs(filters, setFilters);
+  const filterFuncs = getFilterFuncs(filters, setFilters)
 
-    const filterDatas = titles.reduce((accum: IFilterData[], title) => {
-        const { filter, selectFilter } = filterFuncs(title);
-        const result = { filter, selectFilter };
-        accum.push(result);
-        return accum;
-    }, []);
+  const filterDatas = titles.reduce((accum: IFilterData[], title) => {
+    const { filter, selectFilter } = filterFuncs(title)
+    const result = { filter, selectFilter }
+    accum.push(result)
+    return accum
+  }, [])
 
-    const expandTabFilter = (title: IFilterTitle) => () =>
-        setFilters((prev) =>
-            prev.map((filter) => ({
-                ...filter,
-                isExpand: filter.title === title ? false : filter.isExpand,
-            }))
-        );
+  const expandTabFilter = (title: IFilterTitle) => () =>
+    setFilters(prev =>
+      prev.map(filter => ({
+        ...filter,
+        isExpand: filter.title === title ? false : filter.isExpand,
+      }))
+    )
 
-    const getFilterData = (title: IFilterTitle) =>
-        filterDatas.find((func) => func.filter?.title === title)!;
+  const getFilterData = (title: IFilterTitle) =>
+    filterDatas.find(func => func.filter?.title === title)!
 
-    return { expandTabFilter, getFilterData };
-};
+  return { expandTabFilter, getFilterData }
+}
