2d7f28b453bc20d0c710f13dd8af5b38d5684b71
Hapnees <8dwardm8lton@gmail.com>
2023-05-04 11:21:38 +0300

    Feat (stories): remove extra lines

--- src/stories/UI/HighlightButton/HighlightButton.stories.tsx
+++ src/stories/UI/HighlightButton/HighlightButton.stories.tsx
@@ -2,42 +2,40 @@ import HighlightButton from '@/components/UI/HighlightButton/HighlightButton'
 import { Meta, StoryObj } from '@storybook/react'
 import { CSSProperties } from 'react'
 
-type Story = StoryObj<typeof HighlightButton>
-
 const meta: Meta = {
-	title: 'UI/HighlightButton',
-	parameters: {
-		docs: {
-			description: {
-				component: 'Самая часто используемая кнопка сайта',
-			},
-		},
-	},
-	component: HighlightButton,
-	argTypes: {
-		children: {
-			name: 'label',
-			description: 'Текст кнопки',
-		},
-		backgroundColor: {
-			name: 'color',
-			description: 'Цвет кнопки',
-			control: 'color',
-		},
-	},
+  title: 'UI/HighlightButton',
+  parameters: {
+    docs: {
+      description: {
+        component: 'Самая часто используемая кнопка сайта',
+      },
+    },
+  },
+  component: HighlightButton,
+  argTypes: {
+    children: {
+      name: 'label',
+      description: 'Текст кнопки',
+    },
+    backgroundColor: {
+      name: 'color',
+      description: 'Цвет кнопки',
+      control: 'color',
+    },
+  },
 }
 
 export const Primary = ({
-	backgroundColor,
-	children,
+  backgroundColor,
+  children,
 }: {
-	backgroundColor: CSSProperties['backgroundColor']
-	children: string
+  backgroundColor: CSSProperties['backgroundColor']
+  children: string
 }) => <HighlightButton style={{ backgroundColor }}>{children}</HighlightButton>
 
 Primary.args = {
-	backgroundColor: '#ea003d',
-	children: 'Смотреть по подписке',
+  backgroundColor: '#ea003d',
+  children: 'Смотреть по подписке',
 }
 
 export default meta
