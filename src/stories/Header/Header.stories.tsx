4c88d0036f968610f002bef39905b1a93cfa6df2
Hapnees <8dwardm8lton@gmail.com>
2023-05-04 11:21:38 +0300

    Feat (stories): remove extra lines

--- src/stories/Header/Header.stories.tsx
+++ src/stories/Header/Header.stories.tsx
@@ -1,20 +1,30 @@
 import Header from '@/components/Header/Header'
+import { store } from '@/store'
 import { Meta, StoryObj } from '@storybook/react'
+import { Provider } from 'react-redux'
 
 type Story = StoryObj<typeof Header>
 
 const meta: Meta = {
-	title: 'Header',
-	parameters: {
-		docs: {
-			description: {
-				component: 'Шапка сайта',
-			},
-		},
-	},
-	component: Header,
+  title: 'Header',
+  parameters: {
+    docs: {
+      description: {
+        component: 'Шапка сайта',
+      },
+    },
+  },
+  component: Header,
 }
 
-export const Primary: Story = {}
+export const Primary: Story = {
+  render: () => (
+    <Provider store={store}>
+      <div style={{ width: 1240 }}>
+        <Header />
+      </div>
+    </Provider>
+  ),
+}
 
 export default meta
--- src/stories/UI/HeaderIconButton/HeaderIconButton.stories.tsx
+++ src/stories/UI/HeaderIconButton/HeaderIconButton.stories.tsx
@@ -1,48 +1,46 @@
 import HeaderIconButton from '@/components/UI/HeaderIconButton/HeaderIconButton'
 import { Meta, StoryObj } from '@storybook/react'
-import { MdOutlineNotifications } from 'react-icons/md'
-import { RiSearchLine } from 'react-icons/ri'
 
 type Story = StoryObj<typeof HeaderIconButton>
 
 const meta: Meta = {
-	title: 'UI/HeaderIconButton',
-	parameters: {
-		docs: {
-			description: {
-				component: 'Кнопка с иконкой для шапки сайта',
-			},
-		},
-	},
-	component: HeaderIconButton,
-	argTypes: {
-		children: {
-			name: 'label',
-			description: 'Текст кнопки',
-		},
-		icon: {
-			description: 'Вариант иконки',
-			control: 'select',
-		},
-		customIcon: {
-			table: {
-				disable: true,
-			},
-		},
-	},
+  title: 'UI/HeaderIconButton',
+  parameters: {
+    docs: {
+      description: {
+        component: 'Кнопка с иконкой для шапки сайта',
+      },
+    },
+  },
+  component: HeaderIconButton,
+  argTypes: {
+    children: {
+      name: 'label',
+      description: 'Текст кнопки',
+    },
+    icon: {
+      description: 'Вариант иконки',
+      control: 'select',
+    },
+    customIcon: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 }
 
 export const Search: Story = {
-	args: {
-		children: 'Поиск',
-		icon: 'search',
-	},
+  args: {
+    children: 'Поиск',
+    icon: 'search',
+  },
 }
 
 export const Notification: Story = {
-	args: {
-		icon: 'notification',
-	},
+  args: {
+    icon: 'notification',
+  },
 }
 
 export default meta
