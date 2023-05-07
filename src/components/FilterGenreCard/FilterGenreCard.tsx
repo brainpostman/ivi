b4f1b7f6f205829306a3b0d47b114d33c0196d5b
Hapnees <8dwardm8lton@gmail.com>
2023-05-08 00:55:30 +0300

    Feat (FilterGenreCard): add event for click

--- src/components/FilterGenreCard/FilterGenreCard.tsx
+++ src/components/FilterGenreCard/FilterGenreCard.tsx
@@ -1,13 +1,15 @@
 import { FC } from 'react'
 import style from './FilterGenreCard.module.scss'
+import { IFilterData } from '@/types/filterBlock.interface'
 
 interface IProps {
   title: string
+  onClick: () => void
 }
 
-const FilterGenreCard: FC<IProps> = ({ title }) => {
+const FilterGenreCard: FC<IProps> = ({ title, onClick }) => {
   return (
-    <div className={style.wrapper}>
+    <div onClick={onClick} className={style.wrapper}>
       <p>{title}</p>
     </div>
   )
