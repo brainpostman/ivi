1e49442ffd6872cea5b2a1ddb3745d59d0b1ab0c
Hapnees <8dwardm8lton@gmail.com>
2023-05-08 23:31:17 +0300

    Feat (films.api.interface): add IStaff; add fields for IFilmsGetResponse

--- src/types/films.api.interface.ts
+++ src/types/films.api.interface.ts
@@ -1,35 +1,45 @@
+export interface IStaff {
+  id: number
+  name: string
+}
+
 export interface IFilmsGetRequest {
-    order?: string;
-    page?: number;
-    take?: number;
-    orderBy?: string;
-    genres?: string[];
-    genres_en?: string[];
-    countries?: string[];
-    actors?: string[];
-    directors?: string[];
+  order?: string
+  page?: number
+  take?: number
+  orderBy?: string
+  genres?: string[]
+  genres_en?: string[]
+  countries?: string[]
+  actors?: string[]
+  directors?: string[]
 }
 
 export interface IFilmsgGetResponse {
-    id: number;
-    name: string;
-    name_en: string;
-    year: number;
-    countries: { name: string }[];
-    genres: { name: string; name_en: string }[];
-    tagline: string;
-    directors: { name: string }[];
-    artists: { name: string }[];
-    age: string;
-    description: string;
-    mainImg: string;
-    time: string;
-    premiereRU: string;
-    premiere: string;
+  id: number
+  name: string
+  name_en: string
+  year: number
+  countries: { name: string }[]
+  genres: { name: string; name_en: string }[]
+  tagline: string
+  directors: IStaff[]
+  artists: IStaff[]
+  actors: IStaff[]
+  montages: IStaff[]
+  compositors: IStaff[]
+  scenario: IStaff[]
+  operators: IStaff[]
+  age: string
+  description: string
+  mainImg: string
+  time: string
+  premiereRU: string
+  premiere: string
 }
 
-export interface IMovie extends Omit<IFilmsgGetResponse, 'countries' | 'genres' | 'directors'> {
-    countries: string;
-    genres: string;
-    directors: string;
+export interface IMovie
+  extends Omit<IFilmsgGetResponse, 'countries' | 'genres'> {
+  countries: string
+  genres: string
 }
