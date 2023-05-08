103226e326f48bf308408f4a0299879a695ccb04
Hapnees <8dwardm8lton@gmail.com>
2023-05-08 23:32:00 +0300

    Feat (films.transform): add mainImg; delete excess lines

--- src/api/transforms/films.transform.ts
+++ src/api/transforms/films.transform.ts
@@ -1,13 +1,13 @@
+import { formatImgUrl } from '@/formatters/imgUrl.format'
 import { IFilmsgGetResponse } from '@/types/films.api.interface'
 
 export const transformFilms = (filmsData: IFilmsgGetResponse) => {
   const result = {
     ...filmsData,
+    mainImg: formatImgUrl(filmsData.mainImg),
     countries:
       filmsData.countries?.map(country => country.name).join(',') || '',
     genres: filmsData.genres?.map(genre => genre.name).join(',') || '',
-    directors:
-      filmsData.directors?.map(director => director.name).join(',') || '',
   }
 
   return result
