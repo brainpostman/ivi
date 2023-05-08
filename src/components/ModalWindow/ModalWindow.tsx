2975c4b39012eee4d3f290a981965412040366ce
Hapnees <8dwardm8lton@gmail.com>
2023-05-09 00:01:42 +0300

    Feat (ModalWindow): add overflow property

--- src/components/ModalWindow/ModalWindow.tsx
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -1,26 +1,34 @@
-import { DetailedHTMLProps, FC, HTMLAttributes, KeyboardEvent } from 'react'
+import {
+  DetailedHTMLProps,
+  FC,
+  HTMLAttributes,
+  KeyboardEvent,
+  useEffect,
+  useRef,
+} from 'react'
 import { IoCloseOutline } from 'react-icons/io5'
 import style from './ModalWindow.module.scss'
+import useIsomorphicLayoutEffect from '@/hooks/useIsomorphicLayoutEffect'
 
 interface IProps
-	extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
-	isShow?: boolean
-	closeFunc: () => void
+  extends DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> {
+  isShow: boolean
+  closeFunc: () => void
 }
 
 const ModalWindow: FC<IProps> = ({ children, isShow, closeFunc, ...props }) => {
-	const onKeyPressEsc = (event: KeyboardEvent<HTMLElement>) => {
-		if (event.key === 'Escape') closeFunc()
-	}
+  const onKeyPressEsc = (event: KeyboardEvent<HTMLElement>) => {
+    if (event.key === 'Escape') closeFunc()
+  }
 
-	return (
-		<section
-			className={`${style.wrapper} ${!isShow ? style.hide : ''}`}
-			{...props}
-		>
-			<IoCloseOutline className={style.close_icon} onClick={closeFunc} />
-			{children}
-		</section>
-	)
+  return (
+    <section
+      className={`${style.wrapper} ${!isShow ? style.hide : ''}`}
+      {...props}
+    >
+      <IoCloseOutline className={style.close_icon} onClick={closeFunc} />
+      {children}
+    </section>
+  )
 }
 export default ModalWindow
