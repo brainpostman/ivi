a03d8f407c4b53e9c8b104dc8a849037ea16dfca
Marat Sattarov <sattarovmk@yandex.ru>
2023-05-10 04:29:48 +0500

    Feat (api) added CRUD-specific get methods

--- src/api/queries/films.api.ts
+++ src/api/queries/films.api.ts
@@ -1,37 +1,54 @@
-import {
-  IFilmsGetRequest,
-  IFilmsgGetResponse,
-  IMovie,
-} from '@/types/films.api.interface'
-import { toast } from 'react-toastify'
-import { transformFilms } from '../transforms/films.transform'
-import { customAxios } from './customAxios'
+import { IFilmsGetRequest, IFilmsgGetResponse, IMovie } from '@/types/films.api.interface';
+import { toast } from 'react-toastify';
+import { transformFilms } from '../transforms/films.transform';
+import { customAxios } from './customAxios';
+import { CrudFilm, ICrudDetailedFilm, ICrudFilm, IFilmJson } from '@/types/ICrudMovie';
 
 export const filmsAPI = {
-  getFilms(params?: IFilmsGetRequest) {
-    return getFilms(params)
-  },
-  getFilmsHomePage(params?: { page?: number }) {
-    return getFilms({ take: 19, page: params?.page || 1 })
-  },
-}
+    getFilms(params?: IFilmsGetRequest) {
+        return getFilms(params);
+    },
+    getFilmsHomePage(params?: { page?: number }) {
+        return getFilms({ take: 19, page: params?.page || 1 });
+    },
+    getCrudFilms(params?: IFilmsGetRequest) {
+        return getCrudFilms(params);
+    },
+};
 
 const getFilms = async (
-  params?: IFilmsGetRequest
+    params?: IFilmsGetRequest
 ): Promise<{ films: IMovie[]; totalCount: number }> => {
-  try {
-    const filmsData = await customAxios.get<IFilmsgGetResponse[]>('/films', {
-      params,
-    })
+    try {
+        const filmsData = await customAxios.get<IFilmsgGetResponse[]>('/films', {
+            params,
+        });
+        const films = filmsData.data.map((film) => transformFilms(film));
+        const totalCount = filmsData.headers['x-total-count'];
 
-    const films = filmsData.data.map(film => transformFilms(film))
-    const totalCount = filmsData.headers['x-total-count']
+        console.log(films);
 
-    console.log(films)
+        return { films, totalCount };
+    } catch (error) {
+        toast.error('Ошибка при получении фильмов!');
+        return { films: [], totalCount: 0 };
+    }
+};
 
-    return { films, totalCount }
-  } catch (error) {
-    toast.error('Ошибка при получении фильмов!')
-    return { films: [], totalCount: 0 }
-  }
-}
+const getCrudFilms = async (
+    params?: IFilmsGetRequest
+): Promise<{ films: ICrudFilm[]; totalCount: number }> => {
+    try {
+        const response = await customAxios.get<IFilmJson[]>('/films', {
+            params,
+        });
+        const films = response.data.map((film) => {
+            return new CrudFilm(film);
+        });
+        const totalCount = response.headers['x-total-count'];
+        return { films, totalCount };
+    } catch (error: any) {
+        toast.error(error);
+        return { films: [], totalCount: 0 };
+    }
+};
--- src/api/queries/filters.api.ts
+++ src/api/queries/filters.api.ts
@@ -1,82 +1,87 @@
-import { formatCapitalize } from '@/formatters/capitalize.format'
-import { IFilterGetResponse } from '@/types/filters.api.interface'
-import { toast } from 'react-toastify'
-import { transformFilter } from '../transforms/filter.transform'
-import { customAxios } from './customAxios'
+import { formatCapitalize } from '@/formatters/capitalize.format';
+import { IFilterGetResponse } from '@/types/filters.api.interface';
+import { toast } from 'react-toastify';
+import { transformFilter } from '../transforms/filter.transform';
+import { customAxios } from './customAxios';
+import { CrudGenre, ICrudGenre, IGenreJson } from '@/types/ICrudMovie';
 
 export const filtersAPI = {
-  getGenres() {
-    return getGenres()
-  },
-  getCountries() {
-    return getCountries()
-  },
-  getDirectors() {
-    return getDirectors()
-  },
-  getActors() {
-    return getActors()
-  },
-}
+    getGenres(locale: string) {
+        return getGenres(locale);
+    },
+    getCrudGenres() {
+        return getCrudGenres();
+    },
+    getCountries() {
+        return getCountries();
+    },
+    getDirectors() {
+        return getDirectors();
+    },
+    getActors() {
+        return getActors();
+    },
+};
 
-const getGenres = async (): Promise<IFilterGetResponse[]> => {
-  try {
-    const genresData = await customAxios.get<IFilterGetResponse[]>('/genres')
+const getGenres = async (locale: string): Promise<IFilterGetResponse[]> => {
+    try {
+        const genresData = await customAxios.get<IFilterGetResponse[]>('/genres');
 
-    const genres = genresData.data.map(genre =>
-      transformFilter(genre, formatCapitalize)
-    )
-    return genres
-  } catch (error) {
-    toast.error('Ошибка при получении жанров!')
-    return []
-  }
-}
+        const genres = genresData.data.map((genre) => {
+            const name = genre.name_en && locale !== 'ru' ? genre.name_en : genre.name;
+            return transformFilter({ id: genre.id, name }, formatCapitalize);
+        });
+        return genres;
+    } catch (error) {
+        toast.error('Ошибка при получении жанров!');
+        return [];
+    }
+};
+
+const getCrudGenres = async (): Promise<ICrudGenre[]> => {
+    try {
+        const response = await customAxios.get<ICrudGenre[]>('/genres');
+        return response.data;
+    } catch (error) {
+        toast.error('Ошибка при получении жанров!');
+        return [];
+    }
+};
 
 const getCountries = async (): Promise<IFilterGetResponse[]> => {
-  try {
-    const countriesData = await customAxios.get<IFilterGetResponse[]>(
-      '/countries'
-    )
+    try {
+        const countriesData = await customAxios.get<IFilterGetResponse[]>('/countries');
 
-    const countries = countriesData.data.map(country =>
-      transformFilter(country)
-    )
+        const countries = countriesData.data.map((country) => transformFilter(country));
 
-    return countries
-  } catch (error) {
-    toast.error('Ошибка при получении стран!')
-    return []
-  }
-}
+        return countries;
+    } catch (error) {
+        toast.error('Ошибка при получении стран!');
+        return [];
+    }
+};
 
 const getDirectors = async (): Promise<IFilterGetResponse[]> => {
-  try {
-    const directorsData = await customAxios.get<IFilterGetResponse[]>(
-      '/staffs?type=director'
-    )
+    try {
+        const directorsData = await customAxios.get<IFilterGetResponse[]>('/staffs?type=director');
 
-    const directors = directorsData.data.map(director =>
-      transformFilter(director)
-    )
-    return directors
-  } catch (error) {
-    toast.error('Ошибка при получении режиссёров!')
-    return []
-  }
-}
+        const directors = directorsData.data.map((director) => transformFilter(director));
+        return directors;
+    } catch (error) {
+        toast.error('Ошибка при получении режиссёров!');
+        return [];
+    }
+};
 
 const getActors = async (): Promise<IFilterGetResponse[]> => {
-  try {
-    const actorsData = await customAxios.get<IFilterGetResponse[]>(
-      '/staffs?type=actor'
-    )
+    try {
+        const actorsData = await customAxios.get<IFilterGetResponse[]>('/staffs?type=actor');
 
-    const actors = actorsData.data.map(actor => transformFilter(actor))
+        const actors = actorsData.data.map((actor) => transformFilter(actor));
 
-    return actors
-  } catch (error) {
-    toast.error('Ошибка при получении режиссёров!')
-    return []
-  }
-}
+        return actors;
+    } catch (error) {
+        toast.error('Ошибка при получении режиссёров!');
+        return [];
+    }
+};
