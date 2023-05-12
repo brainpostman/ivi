765f489d8742eca50631b1bad6ebd3344b769487
Hapnees <8dwardm8lton@gmail.com>
2023-05-13 00:20:33 +0300

    Feat (CustomCarousel): edit required props

--- src/formatters/carouselWidth.format.ts
+++ src/formatters/carouselWidth.format.ts
@@ -2,31 +2,33 @@ import { ICustomCarouselWidth } from '@/types/customCarousel.interface'
 
 // === PROPS ===
 // @param { * } width - тип ширины карусели
-// @param { * } elementLens - массив длин элементов
-// @param { * } elementsView - количество видимых элементов
 // @param { * } space - отступ между элементами
 // @param padding - внутренний отступ
+// @param  elementLens - массив длин элементов
+// @param  elementsView - количество видимых элементов
 
 export const formatCarouselWidth = (
   width: ICustomCarouselWidth,
-  elementLens: number[],
-  elementsView: number,
   space: number,
-  padding = 0
+  padding = 0,
+  elementLens?: number[],
+  elementsView?: number
 ) => {
   if (width === 'full') return 1225
 
-  const resultWidth =
-    elementLens.slice(0, elementsView).reduce((accum, item) => {
-      if (accum < 1225) accum += item
-      return accum
-    }, 0) -
-    space +
-    padding
+  if (elementsView && elementLens) {
+    const resultWidth =
+      elementLens.slice(0, elementsView).reduce((accum, item) => {
+        if (accum < 1225) accum += item
+        return accum
+      }, 0) -
+      space +
+      padding
 
-  if (width === 'fit') return resultWidth
-  if (width === 'fit-shadow')
-    return resultWidth + space + elementLens[elementsView] / 2 + padding
+    if (width === 'fit') return resultWidth
+    if (width === 'fit-shadow')
+      return resultWidth + space + elementLens[elementsView] / 2 + padding
+  }
 
   return 0
 }
