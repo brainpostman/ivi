12f93e620e4f2ed7951f29216b300c6c5f947305
Marat Sattarov <sattarovmk@yandex.ru>
2023-05-13 20:40:46 +0500

    Feat (admin, movies) added Sort localization

--- src/components/Sort/SortDirection/SortDirection.tsx
+++ src/components/Sort/SortDirection/SortDirection.tsx
@@ -2,6 +2,7 @@ import { useSetListParam } from '@/hooks/useSetListParam';
 import { useEffect } from 'react';
 import { GoArrowLeft } from 'react-icons/go';
 import style from './SortDirection.module.scss';
+import { useTranslation } from 'next-i18next';
 
 const defaultDirections = [
     { id: 1, name: 'ASC', view: 'Вверх', isSelect: false },
@@ -9,11 +10,16 @@ const defaultDirections = [
 ];
 
 const SortDirection = () => {
-    const { list: directions, onClickListEl } = useSetListParam(defaultDirections, 'order', {
-        filterType: 'radio',
-    });
+    const { t } = useTranslation('movies');
+    const { list: directions, onClickListEl } = useSetListParam(
+        t('sortDirections', { returnObjects: true }),
+        'order',
+        {
+            filterType: 'radio',
+        }
+    );
 
-    const currentDicrection = directions.find((el) => el.isSelect);
+    const currentDirection = directions.find((el) => el.isSelect);
     const nextDirection = directions.find((el) => !el.isSelect);
 
     const onClickDirect = (_param: string) => () => {
@@ -24,12 +30,12 @@ const SortDirection = () => {
         onClickListEl('ASC')();
     }, []);
 
-    //if (!currentDicrection) return <></>;
+    if (!currentDirection) return <></>;
 
     return (
         <div className={style.wrapper} onClick={onClickDirect(nextDirection?.name || '')}>
-            <GoArrowLeft className={currentDicrection?.name === 'DESC' ? style.down : ''} />
-            <p>{currentDicrection?.view}</p>
+            <GoArrowLeft className={currentDirection?.name === 'DESC' ? style.down : ''} />
+            <p>{currentDirection?.view}</p>
         </div>
     );
 };
