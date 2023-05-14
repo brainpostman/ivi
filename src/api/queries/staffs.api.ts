86517a0b1e8ea3ddb4e72310200165a9ab9661c7
Hapnees <8dwardm8lton@gmail.com>
2023-05-14 20:06:49 +0300

    Feat (staffs.api): add getStaffById; remove catch

--- src/api/queries/staffs.api.ts
+++ src/api/queries/staffs.api.ts
@@ -1,13 +1,14 @@
 import { formatCapitalize } from '@/formatters/capitalize.format'
 import {
   IFilterGetResponse,
+  IStaff,
   IStaffGetRequest,
   IStaffGetResponse,
 } from '@/types/staffs.interface'
-import { toast } from 'react-toastify'
-import { transformFilter } from '../transforms/filter.transform'
+import { transformStaff } from '../transforms/staff.transform'
 import { customAxios } from './customAxios'
 import { ICrudGenre } from '@/types/ICrudMovie'
+import { displayErrorsServer } from '@/utils/error.util'
 
 export const staffsAPI = {
   getGenres(locale: string, params?: IStaffGetRequest) {
@@ -25,99 +26,74 @@ export const staffsAPI = {
   getActors(params?: IStaffGetRequest) {
     return getActors(params)
   },
+  getStaffById(id: number) {
+    return getStaffById(id)
+  },
 }
 
 const getGenres = async (
   locale: string,
   params?: IStaffGetRequest
-): Promise<IStaffGetResponse[]> => {
-  try {
-    const genresData = await customAxios.get<IStaffGetResponse[]>('/genres', {
-      params,
-    })
+): Promise<IFilterGetResponse[]> => {
+  const genresData = await customAxios.get<IStaffGetResponse[]>('/genres', {
+    params,
+  })
 
-    const genres = genresData.data.map(genre => {
-      const name = genre.name_en && locale !== 'ru' ? genre.name_en : genre.name
-      return transformFilter({ id: genre.id, name }, formatCapitalize)
-    })
-    return genres
-  } catch (error: any) {
-    toast.error(error)
-    return []
-  }
+  const genres = genresData.data.map(genre => {
+    const name = genre.name_en && locale !== 'ru' ? genre.name_en : genre.name
+    return { ...genre, name: formatCapitalize(name) }
+  })
+
+  return genres
 }
 
 const getCrudGenres = async (
   params?: IStaffGetRequest
 ): Promise<ICrudGenre[]> => {
-  try {
-    const response = await customAxios.get<ICrudGenre[]>('/genres', {
-      params,
-    })
-    return response.data
-  } catch (error: any) {
-    toast.error(error)
-    return []
-  }
+  const response = await customAxios.get<ICrudGenre[]>('/genres', {
+    params,
+  })
+  return response.data
 }
 
-const getCountries = async (): Promise<IStaffGetResponse[]> => {
-  try {
-    const countriesData = await customAxios.get<IFilterGetResponse[]>(
-      '/countries'
-    )
+const getCountries = async (): Promise<IFilterGetResponse[]> => {
+  const countriesData = await customAxios.get<IFilterGetResponse[]>(
+    '/countries'
+  )
 
-    const countries = countriesData.data.map(country =>
-      transformFilter(country)
-    )
+  const countries = countriesData.data
 
-    return countries
-  } catch (error: any) {
-    toast.error(error)
-    return []
-  }
+  return countries
 }
 
-const getDirectors = async (
-  params?: IStaffGetRequest
-): Promise<IStaffGetResponse[]> => {
-  try {
-    const directorsData = await customAxios.get<IStaffGetResponse[]>(
-      '/staffs',
-      {
-        params: {
-          type: 'actor',
-          ...params,
-        },
-      }
-    )
+const getDirectors = async (params?: IStaffGetRequest): Promise<IStaff[]> => {
+  const directorsData = await customAxios.get<IStaffGetResponse[]>('/staffs', {
+    params: {
+      type: 'director',
+      ...params,
+    },
+  })
 
-    const directors = directorsData.data.map(director =>
-      transformFilter(director)
-    )
-    return directors
-  } catch (error: any) {
-    toast.error(error)
-    return []
-  }
+  const directors = directorsData.data.map(director => transformStaff(director))
+  return directors
 }
 
-const getActors = async (
-  params?: IStaffGetRequest
-): Promise<IStaffGetResponse[]> => {
-  try {
-    const actorsData = await customAxios.get<IStaffGetResponse[]>('/staffs', {
-      params: {
-        type: 'actor',
-        ...params,
-      },
-    })
+const getActors = async (params?: IStaffGetRequest): Promise<IStaff[]> => {
+  const actorsData = await customAxios.get<IStaffGetResponse[]>('/staffs', {
+    params: {
+      type: 'actor',
+      ...params,
+    },
+  })
 
-    const actors = actorsData.data.map(actor => transformFilter(actor))
+  const actors = actorsData.data.map(actor => transformStaff(actor))
 
-    return actors
-  } catch (error: any) {
-    toast.error(error)
-    return []
-  }
+  return actors
+}
+
+const getStaffById = async (id: number): Promise<IStaff | undefined> => {
+  const staff = (await customAxios.get(`/staffs/${id}`)).data
+  const formattedStaff = transformStaff(staff)
+
+  return formattedStaff
 }
