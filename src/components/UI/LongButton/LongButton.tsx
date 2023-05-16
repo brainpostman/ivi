7e0c1f342d90161c79476bcc88c0fbe36818bfb9
Hapnees <8dwardm8lton@gmail.com>
2023-05-16 14:38:38 +0300

    ***

--- src/components/UI/LongButton/LongButton.tsx
+++ src/components/UI/LongButton/LongButton.tsx
@@ -4,29 +4,29 @@ import { ButtonHTMLAttributes, DetailedHTMLProps, FC } from 'react'
 import style from './LongButton.module.scss'
 
 interface IProps
-	extends DetailedHTMLProps<
-		ButtonHTMLAttributes<HTMLButtonElement>,
-		HTMLButtonElement
-	> {
-	variant?: IVarButton
-	img?: string
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  variant?: IVarButton
+  img?: string
 }
 
 const LongButton: FC<IProps> = ({
-	variant = 'primary',
-	img,
-	children,
-	...props
+  variant = 'primary',
+  img,
+  children,
+  ...props
 }) => {
-	const additClassName = variant === 'secondary' ? style.secondary : ''
-	const className = `${style.button} ${additClassName}`
+  const additClassName = variant === 'secondary' ? style.secondary : ''
+  const className = `${style.button} ${additClassName}`
 
-	return (
-		<button className={className} {...props}>
-			{img && <Image src={img} alt='' width={24} height={32} />}
-			<p>{children}</p>
-		</button>
-	)
+  return (
+    <button className={className} {...props}>
+      {img && <Image src={img} alt='' width={24} height={32} />}
+      <p>{children}</p>
+    </button>
+  )
 }
 
 export default LongButton
