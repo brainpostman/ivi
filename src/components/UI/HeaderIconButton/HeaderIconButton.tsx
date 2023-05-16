de9b2c5bb17434e9103f2af0d44da7e3d2be9c30
Hapnees <8dwardm8lton@gmail.com>
2023-05-16 15:26:22 +0300

    Feat (HeaderIconButton): change wrapper tag from <div> to <button>

--- src/components/UI/HeaderIconButton/HeaderIconButton.tsx
+++ src/components/UI/HeaderIconButton/HeaderIconButton.tsx
@@ -1,42 +1,44 @@
 import { IIconHedaderIconButton } from '@/types/button.interface'
-import { DetailedHTMLProps, FC, HTMLAttributes } from 'react'
+import { ButtonHTMLAttributes, DetailedHTMLProps, FC } from 'react'
 import { MdOutlineNotifications } from 'react-icons/md'
 import { RiSearchLine } from 'react-icons/ri'
 import style from './HeaderIconButton.module.scss'
 
 interface IProps
-	extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-	icon: IIconHedaderIconButton
-	customIcon?: React.ReactElement
+  extends DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  icon?: IIconHedaderIconButton
+  customIcon?: React.ReactElement
 }
 
 const HeaderIconButton: FC<IProps> = ({
-	children,
-	icon,
-	customIcon,
-	...props
+  children,
+  icon,
+  customIcon,
+  className: classNameIncoming = '',
+  ...props
 }) => {
-	const className = `text ${style.wrapper}`
+  const className = `text ${style.wrapper} ${classNameIncoming}`
 
-	const getIcon = () => {
-		switch (icon) {
-			case 'search':
-				return <RiSearchLine />
-			case 'notification':
-				return <MdOutlineNotifications className={style.notif_icon} />
-			default:
-				return customIcon || <></>
-		}
-	}
+  const getIcon = () => {
+    switch (icon) {
+      case 'search':
+        return <RiSearchLine />
+      case 'notification':
+        return <MdOutlineNotifications className={style.notif_icon} />
+      default:
+        return customIcon || <></>
+    }
+  }
 
-	return (
-		<div className={className} {...props}>
-			<div className={style.container}>
-				{getIcon()}
-				{children}
-			</div>
-		</div>
-	)
+  return (
+    <button className={className} {...props}>
+      {getIcon()}
+      <p>{children}</p>
+    </button>
+  )
 }
 
 export default HeaderIconButton
