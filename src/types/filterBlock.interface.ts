b8f0c920b0a27a0f60a29867b26f44a9b03d0c8d
Hapnees <8dwardm8lton@gmail.com>
2023-05-16 21:58:04 +0300

    Feat (FilterBlock): remove FilterListSmall (filter by years)

--- src/types/filterBlock.interface.ts
+++ src/types/filterBlock.interface.ts
@@ -1,21 +1,25 @@
-import { IStaffGetResponse } from './staffs.interface'
-
-export type IFilterTitle = 'genres' | 'country' | 'year' | 'rating' | 'director' | 'actor' | string;
+export type IFilterTitle =
+  | 'genres'
+  | 'country'
+  | 'rating'
+  | 'director'
+  | 'actor'
+  | string
 
 export interface IFilterBlockEl {
-    title: IFilterTitle;
-    isExpand: boolean;
+  title: IFilterTitle
+  isExpand: boolean
 }
 
 export interface IFilterListEl {
-    title: string;
-    param: string;
-    isSelect: boolean;
+  title: string
+  param: string
+  isSelect: boolean
 }
 
 export interface IFilterData {
-    filter: IFilterBlockEl | undefined;
-    selectFilter: () => void;
+  filter: IFilterBlockEl | undefined
+  selectFilter: () => void
 }
 
 export interface IFilterListBigProps {
@@ -34,25 +38,25 @@ export interface IFilterListSmallProps {
 }
 
 export interface IFilterSuggestProps {
-    filterData: IFilterData;
-    closeModal: () => void;
-    suggestList: string[];
-    placeholder?: string;
-    query: string;
+  filterData: IFilterData
+  closeModal: () => void
+  suggestList: string[]
+  placeholder?: string
+  query: string
 }
 
 export interface IFilterSliderProps {
-    title: string;
-    query: string;
-    range?: {
-        min: number;
-        max: number;
-    };
-    maxValue?: number;
+  title: string
+  query: string
+  range?: {
+    min: number
+    max: number
+  }
+  maxValue?: number
 }
 
 export interface ISortType {
-    id: number;
-    name: string;
-    view: string;
+  id: number
+  name: string
+  view: string
 }
