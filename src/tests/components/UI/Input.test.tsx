0e768fde0347e0d28b527376b0a0b16189026109
Hapnees <8dwardm8lton@gmail.com>
2023-05-16 15:26:44 +0300

    Feat (tests/.../UI): add Input tests

---
+++ src/tests/components/UI/Input.test.tsx
@@ -0,0 +1,22 @@
+import Input from '@/components/UI/Input/Input'
+import { renderModif } from '@/utils/test-utils/renderModif.util'
+import '@testing-library/jest-dom'
+import { fireEvent } from '@testing-library/react'
+
+const testValue = 'test'
+
+describe('<Input />', () => {
+  // Проверяем ввод текста
+  it('Check change text', () => {
+    const { container } = renderModif(<Input />)
+
+    const input = container.getElementsByTagName('input')[0]
+    expect(input).toBeInTheDocument()
+
+    expect(input).toHaveDisplayValue('')
+
+    fireEvent.change(input, { target: { value: testValue } })
+
+    expect(input).toHaveDisplayValue(testValue)
+  })
+})
