e2e5ce52dbf04e83b6a270f9f60480f5ecdaccb6
Hapnees <8dwardm8lton@gmail.com>
2023-05-16 14:37:11 +0300

    Feat (BasicBtn): rework href

--- src/components/UI/BasicBtn/BasicBtnWrapper/BasicBtnWrapper.tsx
+++ src/components/UI/BasicBtn/BasicBtnWrapper/BasicBtnWrapper.tsx
@@ -3,23 +3,16 @@ import { FC } from 'react'
 import { IBasicBtnProps } from '../BasicBtn.interface'
 
 const BasicBtnWrapper: FC<
-	Pick<
-		IBasicBtnProps,
-		'href' | 'children' | 'className' | 'target' | 'navlink' | 'onClick'
-	>
-> = ({ href, navlink, target, className, onClick, children }) =>
-	href ? (
-		<a href={href} target={target} className={className} rel='noreferrer'>
-			{children}
-		</a>
-	) : navlink ? (
-		<Link href={navlink} className={className}>
-			{children}
-		</Link>
-	) : (
-		<button className={className} onClick={onClick}>
-			{children}
-		</button>
-	)
+  Pick<IBasicBtnProps, 'href' | 'children' | 'className' | 'target' | 'onClick'>
+> = ({ href, target, className, onClick, children }) =>
+  href ? (
+    <Link href={href} className={className} target={target} rel='noreferrer'>
+      {children}
+    </Link>
+  ) : (
+    <button className={className} onClick={onClick}>
+      {children}
+    </button>
+  )
 
 export default BasicBtnWrapper
