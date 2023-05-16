b498c5bdf74b88907ae310d4bbc83b7e464e5265
Hapnees <8dwardm8lton@gmail.com>
2023-05-16 20:48:41 +0300

    Feat (films.api.test): split suite tests

--- src/tests/api/queries/films.api.test.ts
+++ src/tests/api/queries/films.api.test.ts
@@ -116,31 +116,3 @@ describe('API-FILMS', () => {
     )
   })
 })
-
-describe('FILMS API ERRORS', () => {
-  let errorFilmsData: { films: IMovie[]; totalCount: number }
-
-  let errorFilmById: IMovieById | undefined
-  let errorCrudFilmsData: { films: ICRUDMovie[]; totalCount: number }
-
-  beforeAll(async () => {
-    errorFilmsData = await filmsAPI.getFilms({ page: -1 })
-    errorFilmById = await filmsAPI.getFilmsById(-1)
-    errorCrudFilmsData = await filmsAPI.getCrudFilms({ page: -1 })
-  })
-
-  // Проверяем ошибку при получении фильмов
-  it('Check films error', () => {
-    expect(errorFilmsData).toStrictEqual({ films: [], totalCount: 0 })
-  })
-
-  // Проверяем ошибку при получении фильма по id
-  it('Check film by id error', () => {
-    expect(!errorFilmById).toBeTruthy()
-  })
-
-  // Проверяем ошибку при получении круда фильмов
-  it('Check crud films error', () => {
-    expect(errorCrudFilmsData).toStrictEqual({ films: [], totalCount: 0 })
-  })
-})
