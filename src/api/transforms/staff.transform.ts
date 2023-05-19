40cb3a3c077951db395a883cc2c21c11b7fe1ebb
Hapnees <8dwardm8lton@gmail.com>
2023-05-20 01:26:05 +0300

    Feat (staff.interface): use types; add comments

--- src/api/transforms/staff.transform.ts
+++ src/api/transforms/staff.transform.ts
@@ -1,7 +1,17 @@
-import { IStaff, IStaffGetResponse } from '@/types/staffs.interface'
+import { IStaff, IStaffGetResponse, IStaffType } from '@/types/staffs.interface'
+
+/*
+  * Преобразуем данные об участнике
+  * @param {IStaffGetResponse} staffData - данные об участнике
+  * @returns IStaff - преобразованные данные об участнике
+
+*/
 
 export const transformStaff = (staffData: IStaffGetResponse): IStaff => {
-  const result = { ...staffData, type: staffData.types[0].name }
+  const result: IStaff = {
+    ...staffData,
+    type: staffData.types[0].name as IStaffType,
+  }
 
   return result
 }
