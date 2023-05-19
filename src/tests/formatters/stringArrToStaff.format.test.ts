76c96e1e1f51c2f0915515a39e49c3958823b1c5
Hapnees <8dwardm8lton@gmail.com>
2023-05-20 01:23:38 +0300

    Feat (tests/formatters): add stringArrToStaff

---
+++ src/tests/formatters/stringArrToStaff.format.test.ts
@@ -0,0 +1,33 @@
+import formatStringArrToStaff from '@/formatters/stringArrToStaff.format'
+import { IStaffGetResponse, IStaffType } from '@/types/staffs.interface'
+
+const testStaffNames = ['actor1', 'actor2', 'actor3']
+const testStaffType: IStaffType = 'actor'
+
+const expectedTestStaffNames: IStaffGetResponse[] = [
+  {
+    id: 1,
+    name: 'actor1',
+    biography: 'Биография',
+    types: [{ name: testStaffType }],
+  },
+  {
+    id: 2,
+    name: 'actor2',
+    biography: 'Биография',
+    types: [{ name: testStaffType }],
+  },
+  {
+    id: 3,
+    name: 'actor3',
+    biography: 'Биография',
+    types: [{ name: testStaffType }],
+  },
+]
+
+describe('FORMAT stringArrToStaff', () => {
+  it('Valid values', () => {
+    const formattedValue = formatStringArrToStaff(testStaffNames, testStaffType)
+    expect(formattedValue).toStrictEqual(expectedTestStaffNames)
+  })
+})
