f3fb3981cd2a0fa1cb02ff0c3033306d84a0c702
Hapnees <8dwardm8lton@gmail.com>
2023-05-20 01:30:35 +0300

    Feat (stories/.../MoviesPage): add mdx & controls

--- src/stories/pages/MoviesPage/MoviesPage.stories.tsx
+++ src/stories/pages/MoviesPage/MoviesPage.stories.tsx
@@ -9,8 +9,9 @@ import { filterDirectorData } from '@/data/filterDirectordata'
 import { filterActorData } from '@/data/filterActor.data'
 import { filmsListData } from '@/data/films.data'
 import { transformFilms } from '@/api/transforms/films.transform'
-
-type Story = StoryObj<typeof MoviesPage>
+import formatStringArrToStaff from '@/formatters/stringArrToStaff.format'
+import formatStringArrToFilters from '@/formatters/stringArrToFilters.format'
+import { IMovie } from '@/types/films.api.interface'
 
 const meta: Meta = {
   title: 'pages/MoviesPage',
@@ -22,25 +23,78 @@ const meta: Meta = {
     },
   },
   component: MoviesPage,
-  argTypes: {},
+  argTypes: {
+    defaultFilms: { table: { disable: true } },
+    totalCount: { table: { disable: true } },
+    actors: {
+      description: 'Список актёров (через запятую)',
+    },
+    directors: {
+      description: 'Список режиссёров (через запятую)',
+    },
+    genres: {
+      description: 'Список жанров (через запятую)',
+    },
+    countries: {
+      description: 'Список стран (через запятую)',
+    },
+  },
 }
 
-export const Primary: Story = {
-  render: props => (
+interface IPrimary {
+  actors: string
+  directors: string
+  genres: string
+  countries: string
+  defaultFilms: IMovie[]
+  totalCount: number
+  minYear: number
+  maxYear: number
+  minCountScore: number
+  maxCountScore: number
+}
+
+export const Primary = ({
+  actors: actorsIncoming,
+  countries: countriesIncoming,
+  directors: directorsIncoming,
+  genres: genresIncoming,
+  ...props
+}: IPrimary) => {
+  const actors = formatStringArrToStaff(actorsIncoming.split(','), 'actor')
+  const directors = formatStringArrToStaff(
+    directorsIncoming.split(','),
+    'director'
+  )
+  const countries = formatStringArrToFilters(countriesIncoming.split(','))
+  const genres = formatStringArrToFilters(genresIncoming.split(','))
+
+  return (
     <SessionProvider>
       <Provider store={store}>
-        <MoviesPage {...props} />
+        <MoviesPage
+          actors={actors}
+          directors={directors}
+          countries={countries}
+          genres={genres}
+          {...props}
+        />
       </Provider>
     </SessionProvider>
-  ),
-  args: {
-    defaultFilms: filmsListData.map(film => transformFilms(film)),
-    actors: filterActorData,
-    directors: filterDirectorData,
-    countries: filterCountryListData,
-    genres: filterGenreListData,
-    totalCount: 0,
-  },
+  )
+}
+
+Primary.args = {
+  actors: 'actor1',
+  directors: 'director1',
+  countries: 'country1',
+  genres: 'genre1',
+  defaultFilms: filmsListData.map(film => transformFilms(film)),
+  totalCount: filmsListData.length,
+  minYear: 1996,
+  maxYear: 2023,
+  minCountScore: 74396,
+  maxCountScore: 497326,
 }
 
 export default meta
