74b86d535dbe6fa01cd589272d26635e4239cedc
Hapnees <8dwardm8lton@gmail.com>
2023-05-20 01:22:15 +0300

    Feat (tests/.../films): add films tests

---
+++ src/tests/api/queries/films/films-error.api.test.ts
@@ -0,0 +1,40 @@
+import { filmsAPI } from '@/api/queries/films.api'
+import { ICRUDMovie } from '@/types/ICrudMovie'
+import { IMovie, IMovieById } from '@/types/films.api.interface'
+
+const expectedFilmsError = {
+  films: [],
+  totalCount: 0,
+  minYear: 0,
+  maxYear: 0,
+  minCountScore: 0,
+  maxCountScore: 0,
+}
+
+describe('FILMS-API ERRORS', () => {
+  let errorFilmsData: { films: IMovie[]; totalCount: number }
+
+  let errorFilmById: IMovieById | undefined
+  let errorCrudFilmsData: { films: ICRUDMovie[]; totalCount: number }
+
+  beforeAll(async () => {
+    errorFilmsData = await filmsAPI.getFilms({ page: -1 })
+    errorFilmById = await filmsAPI.getFilmsById(-1)
+    errorCrudFilmsData = await filmsAPI.getCrudFilms({ page: -1 })
+  })
+
+  // Проверяем ошибку при получении фильмов
+  it('Films error', () => {
+    expect(errorFilmsData).toStrictEqual(expectedFilmsError)
+  })
+
+  // Проверяем ошибку при получении фильма по id
+  it('Film by id error', () => {
+    expect(errorFilmById).toBeFalsy()
+  })
+
+  // Проверяем ошибку при получении круда фильмов
+  it('Crud films error', () => {
+    expect(errorCrudFilmsData).toStrictEqual(expectedFilmsError)
+  })
+})
---
+++ src/tests/api/queries/films/films.api.test.ts
@@ -0,0 +1,68 @@
+import { filmsAPI } from '@/api/queries/films.api'
+import {
+  crudFilmsRequiredProperties,
+  filmsRequiredProperites,
+  specificFilmRequiredProperites,
+} from '@/data/requiredProperties.data'
+import { ICRUDMovie } from '@/types/ICrudMovie'
+import { IMovieById } from '@/types/films.api.interface'
+import { IMovie } from '@/types/films.api.interface'
+import { checkObjHaveProperties } from '@/utils/test-utils/checkObjHaveProperties.utils'
+
+// Тестируем API фильмов
+describe('API-FILMS', () => {
+  let films: IMovie[]
+  let totalCount: number
+  let filmById: IMovieById | undefined
+  let homePageFilms: IMovie[]
+
+  let crudFilms: ICRUDMovie[]
+
+  beforeAll(async () => {
+    const filmsData = await filmsAPI.getFilms()
+    films = filmsData.films
+    totalCount = filmsData.totalCount
+    filmById = await filmsAPI.getFilmsById(1)
+
+    homePageFilms = (await filmsAPI.getFilmsHomePage()).films
+    crudFilms = (await filmsAPI.getCrudFilms()).films
+  })
+
+  // Проверяем поля пришедших фильмов
+  it('Films properties', () => {
+    films.forEach(film => checkObjHaveProperties(film, filmsRequiredProperites))
+  })
+
+  //it('Check films totalCount', () => {
+  //  console.log(`totalCount = ${totalCount}`)
+  //  expect(totalCount).toBeTruthy()
+
+  //  const checkType = typeof totalCount === 'number'
+  //  expect(checkType).toBeTruthy()
+  //})
+
+  // Проверяем поля фильма, полученного по id
+  it('Film by id properites', () => {
+    expect(filmById).toBeTruthy()
+
+    if (filmById) {
+      checkObjHaveProperties(filmById, specificFilmRequiredProperites)
+    }
+  })
+
+  // Проверяем фильмы на главной странице
+  it('Films on home page', () => {
+    expect(homePageFilms).toBeTruthy()
+
+    expect(homePageFilms.length === 19).toBeTruthy()
+  })
+
+  // Проверяем круд фильмов
+  it('Crud films', () => {
+    expect(crudFilms).toBeTruthy()
+
+    crudFilms.forEach(crudFilm =>
+      checkObjHaveProperties(crudFilm, crudFilmsRequiredProperties)
+    )
+  })
+})
