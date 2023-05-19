db177fde8a65df536fe19f695b81bd67ad4ce9de
Hapnees <8dwardm8lton@gmail.com>
2023-05-20 02:59:07 +0300

    Feat (staff.transform): rework

--- src/api/transforms/staff.transform.ts
+++ src/api/transforms/staff.transform.ts
@@ -1,17 +1,25 @@
-import { IStaff, IStaffGetResponse, IStaffType } from '@/types/staffs.interface'
+import formatStaffTypestoType from '@/formatters/staffTypesToType.format'
+import { IStaff, IStaffGetResponse } from '@/types/staffs.interface'
 
 /*
   * Преобразуем данные об участнике
+
   * @param {IStaffGetResponse} staffData - данные об участнике
   * @returns IStaff - преобразованные данные об участнике
 
 */
 
-export const transformStaff = (staffData: IStaffGetResponse): IStaff => {
-  const result: IStaff = {
-    ...staffData,
-    type: staffData.types[0].name as IStaffType,
-  }
+export const transformStaff = (staffData: IStaffGetResponse[]): IStaff[] => {
+  const filteredValues = staffData.filter(staff => {
+    const isExistTypesName = !!staff.types[0].name
+    if (!isExistTypesName) {
+      console.error(`Wrong staff type value {undefined}; ID = ${staff.id}`)
+    }
+
+    return isExistTypesName
+  })
+
+  const result = filteredValues.map(staff => formatStaffTypestoType(staff))
 
   return result
 }
