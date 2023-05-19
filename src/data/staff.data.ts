922005f16b33b66d11eb64cf6bc920cc2f86c8ac
Hapnees <8dwardm8lton@gmail.com>
2023-05-20 01:19:42 +0300

    Feat (staff.data): add staffData

---
+++ src/data/staff.data.ts
@@ -0,0 +1,9 @@
+import { IStaff } from '@/types/staffs.interface'
+
+export const staffData: IStaff = {
+  id: 1,
+  name: 'Участник 1',
+  type: 'actor',
+  biography: 'biography',
+  name_en: 'Staff 1',
+}
