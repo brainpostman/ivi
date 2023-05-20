eba9667d888f33fbaec822ef8178be30ae19aa85
Hapnees <8dwardm8lton@gmail.com>
2023-05-20 17:35:46 +0300

    Feat (tests/.../filters): add filters.api

--- src/tests/api/queries/filters/filters.api.test.ts
+++ src/tests/api/queries/filters/filters.api.test.ts
@@ -7,17 +7,22 @@ import { ICRUDGenre } from '@/types/ICrudMovie'
 import { IFilterGetResponse } from '@/types/filters.interface'
 import { checkObjHaveProperties } from '@/utils/test-utils/checkObjHaveProperties.utils'
 
+const ruAlphabet = 'абвгдеёжзийклмнопрстуфхцчшщьыъэюя'.split('')
+const enAlphbet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+
 describe('API-FILTERS', () => {
   let countries: IFilterGetResponse[]
   let genres: IFilterGetResponse[]
+
+  let enGenres: IFilterGetResponse[]
 
   let crudGenres: ICRUDGenre[]
 
   beforeAll(async () => {
     genres = await filtersAPI.getGenres('ru')
     countries = await filtersAPI.getCountries()
-
     crudGenres = await filtersAPI.getCrudGenres()
+    enGenres = await filtersAPI.getGenres('en')
   })
 
   // Проверка полей жанров
@@ -37,5 +42,23 @@ describe('API-FILTERS', () => {
     crudGenres.map(actor =>
       checkObjHaveProperties(actor, crudGenreRequiredProperties)
     )
+  })
+
+  // Проверяем en локаль
+  it('Genre EN locale', () => {
+    const copyEnGenres = [...enGenres]
+    copyEnGenres.filter(enGenre => {
+      const isIncludeRuSymbol = ruAlphabet.some(ruSym =>
+        enGenre.name.includes(ruSym)
+      )
+
+      const isIncludeEnSymbol = enAlphbet.some(enSym =>
+        enGenre.name.includes(enSym)
+      )
+
+      return !isIncludeRuSymbol && isIncludeEnSymbol
+    })
+
+    expect(copyEnGenres.length).toBe(enGenres.length)
   })
 })
