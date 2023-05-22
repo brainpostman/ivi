3e83acbeee07ecf5464c4577bf7a97e72d19ed79
Hapnees <8dwardm8lton@gmail.com>
2023-05-22 22:00:11 +0300

    Feat (coords.utils.test): add test for scrollMove

--- src/tests/utils/coords.utils.test.tsx
+++ src/tests/utils/coords.utils.test.tsx
@@ -1,4 +1,4 @@
-import { getCoordY } from '@/utils/coords.utils'
+import { getCoordY, scrollMove } from '@/utils/coords.utils'
 import { renderModif } from '@/utils/test-utils/renderModif.util'
 import '@testing-library/jest-dom'
 
@@ -6,6 +6,26 @@ let testValidHtmlElement: HTMLElement | null = null
 let testInvalidHtmlElement: HTMLElement | null = null
 
 const testCoordY = 150
+
+const testMove = 200
+const expectedScrollMove = {
+  behavior: 'smooth',
+  top: testMove,
+}
+
+const mockClientRect = {
+  x: 200,
+  y: testCoordY,
+  width: 100,
+  height: 20,
+  top: 200,
+  right: 200,
+  bottom: 250,
+  left: 200,
+  toJSON: jest.fn(),
+}
+
+const scrollMoveSpy = jest.spyOn(window, 'scrollTo').mockImplementation()
 
 describe('UTIL getCoordY', () => {
   beforeAll(() => {
@@ -28,17 +48,7 @@ describe('UTIL getCoordY', () => {
     expect(testValidHtmlElement).toBeInTheDocument()
     if (!testValidHtmlElement) return
 
-    testValidHtmlElement.getBoundingClientRect = jest.fn(() => ({
-      x: 200,
-      y: testCoordY,
-      width: 100,
-      height: 20,
-      top: 200,
-      right: 200,
-      bottom: 250,
-      left: 200,
-      toJSON: jest.fn(),
-    }))
+    testValidHtmlElement.getBoundingClientRect = jest.fn(() => mockClientRect)
 
     const coordY = getCoordY(testValidHtmlElement)
     expect(coordY).toBe(testCoordY)
@@ -58,5 +68,14 @@ describe('UTIL ERROR getCoordY', () => {
   it('Invalid value', () => {
     const coordY = getCoordY(testInvalidHtmlElement)
     expect(coordY).toBe(0)
+  })
+})
+
+describe('UTIL scrollMove', () => {
+  // Пример
+  it('Example', () => {
+    scrollMove(testMove)
+
+    expect(scrollMoveSpy).toHaveBeenCalledWith(expectedScrollMove)
   })
 })
