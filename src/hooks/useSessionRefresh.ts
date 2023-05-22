60ad0e85e238ce428801cecb9a3766d824b903c7
Hapnees <8dwardm8lton@gmail.com>
2023-05-22 21:52:13 +0300

    Feat (useSessionRefresh): spaces

--- src/hooks/useSessionRefresh.ts
+++ src/hooks/useSessionRefresh.ts
@@ -1,26 +1,26 @@
-import { useSession } from 'next-auth/react';
-import { useEffect } from 'react';
+import { useSession } from 'next-auth/react'
+import { useEffect } from 'react'
 
 export function useSessionRefresh() {
-    const { status, data, update } = useSession();
-    let timeout = -1;
-    let currentTime = Date.now();
+  const { status, data, update } = useSession()
+  let timeout = -1
+  let currentTime = Date.now()
 
-    useEffect(() => {
-        if (status === 'authenticated') {
-            if (currentTime >= data.expires_at * 1000) {
-                update();
-            }
-            timeout = window.setTimeout(function callback() {
-                currentTime = Date.now();
-                update();
-                timeout = window.setTimeout(callback, 60 * 60 * 1000);
-            }, data.expires_at * 1000 - currentTime);
-        }
-        return () => {
-            if (timeout > 0) {
-                window.clearTimeout(timeout);
-            }
-        };
-    }, [status]);
+  useEffect(() => {
+    if (status === 'authenticated') {
+      if (currentTime >= data.expires_at * 1000) {
+        update()
+      }
+      timeout = window.setTimeout(function callback() {
+        currentTime = Date.now()
+        update()
+        timeout = window.setTimeout(callback, 60 * 60 * 1000)
+      }, data.expires_at * 1000 - currentTime)
+    }
+    return () => {
+      if (timeout > 0) {
+        window.clearTimeout(timeout)
+      }
+    }
+  }, [status])
 }
