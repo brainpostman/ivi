779ef6d1a4444fcc9989a995399ba72026589728
Marat Sattarov <sattarovmk@yandex.ru>
2023-05-22 22:47:01 +0500

    Fix (api/transforms) fixed transformFilm separator

--- src/api/transforms/films.transform.ts
+++ src/api/transforms/films.transform.ts
@@ -1,14 +1,13 @@
-import { formatImgUrl } from '@/formatters/imgUrl.format'
-import { IFilmsgGetResponse } from '@/types/films.api.interface'
+import { formatImgUrl } from '@/formatters/imgUrl.format';
+import { IFilmsgGetResponse } from '@/types/films.api.interface';
 
 export const transformFilms = <T extends IFilmsgGetResponse>(filmsData: T) => {
-  const result = {
-    ...filmsData,
-    mainImg: formatImgUrl(filmsData.mainImg),
-    countries:
-      filmsData.countries?.map(country => country.name).join(',') || '',
-    genres: filmsData.genres?.map(genre => genre.name).join(',') || '',
-  }
+    const result = {
+        ...filmsData,
+        mainImg: formatImgUrl(filmsData.mainImg),
+        countries: filmsData.countries?.map((country) => country.name).join(', ') || '',
+        genres: filmsData.genres?.map((genre) => genre.name).join(', ') || '',
+    };
 
-  return result
-}
+    return result;
+};
