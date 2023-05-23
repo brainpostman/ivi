925de885666f30305589d13c1d42100b8a21b97c
Marat Sattarov <sattarovmk@yandex.ru>
2023-05-24 01:24:29 +0500

    Feat (utils) added buildDateString for comments

--- src/utils/comment.utils.ts
+++ src/utils/comment.utils.ts
@@ -1,3 +1,5 @@
+import { localizeDateString } from '@/formatters/localizeDateString.format';
+
 export function validateComment(minLength: number, maxLength: number, comment: string): boolean {
     if (comment.length < minLength || comment.length > maxLength) {
         return false;
@@ -8,4 +10,17 @@ export function validateComment(minLength: number, maxLength: number, comment: s
 
 export function trimComment(comment: string) {
     return comment.replace(/\s+/g, ' ').trim();
+}
+
+export function buildDateString(date: Date, locale: string) {
+    const today = new Date();
+    const yesterday = new Date();
+    yesterday.setDate(today.getDate() - 1);
+    if (date.toLocaleDateString() === today.toLocaleDateString()) {
+        return locale === 'ru' ? 'сегодня' : 'today';
+    } else if (date.toLocaleDateString() === yesterday.toLocaleDateString()) {
+        return locale === 'ru' ? 'вчера' : 'yesterday';
+    } else {
+        return localizeDateString(date, locale);
+    }
 }
