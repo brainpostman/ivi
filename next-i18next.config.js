a08956af2c8de00511fbf9f0e819ce0ebd4e4551
Hapnees <8dwardm8lton@gmail.com>
2023-05-24 02:06:29 +0300

    Feat (next-i18next.config): spaces

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-    i18n: {
-        defaultLocale: 'ru',
-        locales: ['ru', 'en'],
-        localeDetection: false,
-    },
-    reloadOnPrerender: true,
-    ns: ['header', 'auth_modal', 'home', 'common', 'footer', 'error', 'movies', 'admin'],
-};
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru', 'en'],
+    localeDetection: false,
+  },
+  reloadOnPrerender: true,
+  ns: [
+    'header',
+    'auth_modal',
+    'home',
+    'common',
+    'footer',
+    'error',
+    'movies',
+    'admin',
+  ],
+}
