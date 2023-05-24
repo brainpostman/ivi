441869cf6765a57a45511486404a86edc1681c9b
Marat Sattarov <sattarovmk@yandex.ru>
2023-05-24 20:39:00 +0500

    Fix (types/api) fixed IReviewGetResponse type key

--- src/types/api/reviews.api.interface.ts
+++ src/types/api/reviews.api.interface.ts
@@ -1,17 +1,17 @@
 export interface IReviewGetResponse {
-  id: number
-  text: string
-  user_id: number
-  film_id: number
-  parent: number | null
-  name: string
-  user_email: string
-  createdAt: string
+    id: number;
+    text: string;
+    user_id: number;
+    film_id: number;
+    parent: number | null;
+    user_name: string;
+    user_email: string;
+    createdAt: string;
 }
 
 export interface IReviewPostRequest {
-  text: string
-  user_id: number
-  film_id: number
-  parent: number | null
+    text: string;
+    user_id: number;
+    film_id: number;
+    parent: number | null;
 }
