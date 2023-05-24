ca2f9070e22021849144b030f932e9150c9c221f
Hapnees <8dwardm8lton@gmail.com>
2023-05-24 12:25:02 +0300

    Feat (films.api.interface): move review types to reviews.api.interface

---
+++ src/types/api/films.api.interface.ts
@@ -0,0 +1,54 @@
+import { IStaffGetResponse } from './staffs.api.interface'
+import { IFilterGetResponse } from './filters.api.interface'
+
+export interface IFilmsGetRequest {
+  order?: string
+  page?: number
+  take?: number
+  orderBy?: string
+  genres?: string[]
+  genres_en?: string[]
+  countries?: string[]
+  actors?: string[]
+  directors?: string[]
+}
+
+export interface IFilmByIdGetResponse extends IFilmsgGetResponse {
+  directors: IStaffGetResponse[]
+  artists: IStaffGetResponse[]
+  actors: IStaffGetResponse[]
+  montages: IStaffGetResponse[]
+  compositors: IStaffGetResponse[]
+  scenario: IStaffGetResponse[]
+  operators: IStaffGetResponse[]
+}
+
+export interface IFilmsgGetResponse {
+  id: number
+  name: string
+  name_en: string
+  year: number
+  countries: IFilterGetResponse[]
+  genres: { id: number; name: string; name_en: string | null }[]
+  tagline: string
+  scoreAVG: number
+  countScore: number
+  age: string
+  description: string
+  mainImg: string
+  time: string
+  premiereRU: string
+  premiere: string
+}
+
+export interface IMovie
+  extends Omit<IFilmsgGetResponse, 'countries' | 'genres'> {
+  countries: string
+  genres: string
+}
+
+export interface IMovieById
+  extends Omit<IFilmByIdGetResponse, 'countries' | 'genres'> {
+  countries: string
+  genres: string
+}
---
+++ src/types/api/reviews.api.interface.ts
@@ -0,0 +1,17 @@
+export interface IReviewGetResponse {
+  id: number
+  text: string
+  user_id: number
+  film_id: number
+  parent: number | null
+  name: string
+  user_email: string
+  createdAt: string
+}
+
+export interface IReviewPostRequest {
+  text: string
+  user_id: number
+  film_id: number
+  parent: number | null
+}
