d7f98b45074f99bf9185c6d8d46b6d86212435ff
Marat Sattarov <sattarovmk@yandex.ru>
2023-05-26 20:14:46 +0500

    Chore (AuthWindow) adaptive layout

--- src/components/AuthWindow/EmailInput/EmailInput.tsx
+++ src/components/AuthWindow/EmailInput/EmailInput.tsx
@@ -1,87 +1,84 @@
-import Input from '@/components/UI/InputFields/Input/Input'
-import parentStyles from '../AuthWindow.module.scss'
-import HighlightButton from '@/components/UI/Buttons/HighlightButton/HighlightButton'
-import BasicBtn from '@/components/UI/Buttons/BasicBtn/BasicBtn'
-import { validateEmail } from '@/utils/auth.util'
-import { signIn } from 'next-auth/react'
-import { FaGoogle, FaVk } from 'react-icons/fa'
-import { ChangeEvent, Dispatch, SetStateAction } from 'react'
-import { useTranslation } from 'next-i18next'
-import styles from './EmailInput.module.scss'
+import Input from '@/components/UI/InputFields/Input/Input';
+import parentStyles from '../AuthWindow.module.scss';
+import HighlightButton from '@/components/UI/Buttons/HighlightButton/HighlightButton';
+import BasicBtn from '@/components/UI/Buttons/BasicBtn/BasicBtn';
+import { validateEmail } from '@/utils/auth.util';
+import { signIn } from 'next-auth/react';
+import { FaGoogle, FaVk } from 'react-icons/fa';
+import { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import { useTranslation } from 'next-i18next';
+import styles from './EmailInput.module.scss';
 
 interface IEmailInputProps {
-  emailInput: string
-  setEmailInput: Dispatch<SetStateAction<string>>
-  errorMessages: string[]
-  setError: (error: string[]) => void
-  resetError: () => void
-  handleEmail: (email: string) => void
-  className?: string
+    emailInput: string;
+    setEmailInput: Dispatch<SetStateAction<string>>;
+    errorMessages: string[];
+    setError: (error: string[]) => void;
+    resetError: () => void;
+    handleEmail: (email: string) => void;
+    className?: string;
 }
 
 const EmailInput = ({
-  emailInput,
-  setEmailInput,
-  errorMessages,
-  setError,
-  resetError,
-  handleEmail,
-  className = '',
+    emailInput,
+    setEmailInput,
+    errorMessages,
+    setError,
+    resetError,
+    handleEmail,
+    className = '',
 }: IEmailInputProps) => {
-  const { t } = useTranslation('auth_modal')
+    const { t } = useTranslation('auth_modal');
 
-  const onClickContinueBtn = () => {
-    let emailError = validateEmail(emailInput)
-    if (emailError) {
-      setError([emailError])
-      return
-    }
-    handleEmail(emailInput)
-  }
+    const onClickContinueBtn = () => {
+        let emailError = validateEmail(emailInput);
+        if (emailError) {
+            setError([emailError]);
+            return;
+        }
+        handleEmail(emailInput);
+    };
 
-  const onChangeEmailInput = (event: ChangeEvent<HTMLInputElement>) => {
-    if (errorMessages.length > 0) resetError()
-    setEmailInput(event.target.value)
-  }
+    const onChangeEmailInput = (event: ChangeEvent<HTMLInputElement>) => {
+        if (errorMessages.length > 0) resetError();
+        setEmailInput(event.target.value);
+    };
 
-  return (
-    <div className={`${styles.container} ${className}`}>
-      <div className={styles.inputs}>
-        <Input
-          type='email'
-          value={emailInput}
-          onChange={event => onChangeEmailInput(event)}
-          placeholder={t('email-placeholder')}
-          autoFocus
-        />
-        <HighlightButton
-          className={parentStyles.highlightbtn}
-          disabled={emailInput ? false : true}
-          onClick={onClickContinueBtn}
-        >
-          {t('continue')}
-        </HighlightButton>
-        <BasicBtn
-          onClick={() => {
-            signIn('google')
-          }}
-          btnType='icon'
-          title={t('login.signin-google')}
-          className={`${styles.basicbtn_google} ${styles.basicbtn}`}
-        >
-          <FaGoogle />
-        </BasicBtn>
-        <BasicBtn
-          onClick={() => signIn('vk')}
-          btnType='icon'
-          title={t('login.signin-vk')}
-          className={`${styles.basicbtn_vk} ${styles.basicbtn}`}
-        >
-          <FaVk />
-        </BasicBtn>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className={`${styles.container} ${className}`}>
+            <div className={styles.inputs}>
+                <Input
+                    type='email'
+                    value={emailInput}
+                    onChange={(event) => onChangeEmailInput(event)}
+                    placeholder={t('email-placeholder')}
+                    autoFocus
+                />
+                <HighlightButton
+                    className={parentStyles.highlightbtn}
+                    disabled={emailInput ? false : true}
+                    onClick={onClickContinueBtn}>
+                    {t('continue')}
+                </HighlightButton>
+                <BasicBtn
+                    onClick={() => {
+                        signIn('google');
+                    }}
+                    btnType='icon'
+                    title={t('login.signin-google')}
+                    className={`${styles.basicbtn_google} ${styles.basicbtn}`}>
+                    <FaGoogle />
+                </BasicBtn>
+                <BasicBtn
+                    onClick={() => signIn('vk')}
+                    btnType='icon'
+                    title={t('login.signin-vk')}
+                    className={`${styles.basicbtn_vk} ${styles.basicbtn}`}>
+                    <FaVk />
+                </BasicBtn>
+            </div>
+        </div>
+    );
+};
 
-export default EmailInput
+export default EmailInput;
--- src/components/AuthWindow/Login/Login.tsx
+++ src/components/AuthWindow/Login/Login.tsx
@@ -1,62 +1,54 @@
-import styles from './Login.module.scss'
-import parentStyles from '../AuthWindow.module.scss'
-import { useTranslation } from 'next-i18next'
-import Input from '@/components/UI/InputFields/Input/Input'
-import { useState } from 'react'
-import HighlightButton from '@/components/UI/Buttons/HighlightButton/HighlightButton'
-import { validatePassword } from '@/utils/auth.util'
+import styles from './Login.module.scss';
+import parentStyles from '../AuthWindow.module.scss';
+import { useTranslation } from 'next-i18next';
+import Input from '@/components/UI/InputFields/Input/Input';
+import { useState } from 'react';
+import HighlightButton from '@/components/UI/Buttons/HighlightButton/HighlightButton';
+import { validatePassword } from '@/utils/auth.util';
 
 interface ILoginProps {
-  errorMessages: string[]
-  setError: (error: string[]) => void
-  handleSignIn: (provider: string, email: string) => void
-  resetError: () => void
+    errorMessages: string[];
+    setError: (error: string[]) => void;
+    handleSignIn: (provider: string, email: string) => void;
+    resetError: () => void;
 }
 
-const Login = ({
-  errorMessages,
-  setError,
-  resetError,
-  handleSignIn,
-}: ILoginProps) => {
-  const { t } = useTranslation('auth_modal')
+const Login = ({ errorMessages, setError, resetError, handleSignIn }: ILoginProps) => {
+    const { t } = useTranslation('auth_modal');
 
-  const [passwordInput, setPasswordInput] = useState('')
+    const [passwordInput, setPasswordInput] = useState('');
 
-  return (
-    <div className={styles.container}>
-      <div
-        className={`${parentStyles.message} ${parentStyles.message__prompt}`}
-      >
-        {t('login.enter-pass-to-login')}
-      </div>
-      <Input
-        type='password'
-        value={passwordInput}
-        onChange={e => {
-          if (errorMessages.length > 0) resetError()
-          setPasswordInput(e.target.value)
-        }}
-        charHideBtn
-        placeholder={t('login.enter-pass')}
-        autoFocus
-      />
-      <HighlightButton
-        disabled={passwordInput ? false : true}
-        className={parentStyles.highlightbtn}
-        onClick={() => {
-          let passwordError = validatePassword(passwordInput)
-          if (passwordError.length > 0) {
-            setError([...passwordError])
-            return
-          }
-          handleSignIn('login', passwordInput)
-        }}
-      >
-        {t('login.login')}
-      </HighlightButton>
-    </div>
-  )
-}
+    return (
+        <div className={styles.container}>
+            <div className={`${parentStyles.message} ${parentStyles.message__prompt}`}>
+                {t('login.enter-pass-to-login')}
+            </div>
+            <Input
+                type='password'
+                value={passwordInput}
+                onChange={(e) => {
+                    if (errorMessages.length > 0) resetError();
+                    setPasswordInput(e.target.value);
+                }}
+                charHideBtn
+                placeholder={t('login.enter-pass')}
+                autoFocus
+            />
+            <HighlightButton
+                disabled={passwordInput ? false : true}
+                className={parentStyles.highlightbtn}
+                onClick={() => {
+                    let passwordError = validatePassword(passwordInput);
+                    if (passwordError.length > 0) {
+                        setError([...passwordError]);
+                        return;
+                    }
+                    handleSignIn('login', passwordInput);
+                }}>
+                {t('login.login')}
+            </HighlightButton>
+        </div>
+    );
+};
 
-export default Login
+export default Login;
