92264fa1073af2300dd821c3b8aee420e4b52dab
Hapnees <8dwardm8lton@gmail.com>
2023-05-27 23:20:11 +0300

    Fix (WatchPage.stories): wrong prop's name

--- src/components/UI/Carousels/BannerCarousel/BannerCarouselItem/BannerCarouselItem.tsx
+++ src/components/UI/Carousels/BannerCarousel/BannerCarouselItem/BannerCarouselItem.tsx
@@ -1,58 +1,62 @@
-import HighlightButton from '@/components/UI/Buttons/HighlightButton/HighlightButton';
-import Image from 'next/image';
-import { IBannerCarouselItem } from '../../../../../types/IBannerCarouselItem';
-import styles from './BannerCarouselItem.module.scss';
-import { useTranslation } from 'next-i18next';
-import Link from 'next/link';
+import HighlightButton from '@/components/UI/Buttons/HighlightButton/HighlightButton'
+import Image from 'next/image'
+import { IBannerCarouselItem } from '../../../../../types/IBannerCarouselItem'
+import styles from './BannerCarouselItem.module.scss'
+import { useTranslation } from 'next-i18next'
+import Link from 'next/link'
 
 interface IBannerCarouselItemProps {
-    item: IBannerCarouselItem;
-    transition: boolean;
-    speed: number;
-    active: boolean;
-    mobile?: boolean;
+  item: IBannerCarouselItem
+  transition: boolean
+  speed: number
+  active: boolean
+  mobile?: boolean
 }
 
 const BannerCarouselItem = ({
-    item,
-    speed = 400,
-    active = false,
-    transition = true,
+  item,
+  speed = 400,
+  active = false,
+  transition = true,
 }: IBannerCarouselItemProps) => {
-    const { t } = useTranslation('home');
+  const { t } = useTranslation('home')
 
-    return (
-        <article
-            className={`${styles.element} ${active ? styles.element_active : ''}`}
+  return (
+    <article
+      className={`${styles.element} ${active ? styles.element_active : ''}`}
+      style={{
+        transition: !transition
+          ? 'none'
+          : `transform ${speed}ms, opacity ${speed}ms`,
+      }}
+    >
+      <Link href={item.catalogLink}>
+        <div className={styles.content}>
+          <h2 className={styles.title}>{item.title}</h2>
+          <h3 className={styles.subtitle}>{item.subtitle}</h3>
+          <div
+            className={`${styles.button} ${active ? styles.button_active : ''}`}
             style={{
-                transition: !transition ? 'none' : `transform ${speed}ms, opacity ${speed}ms`,
-            }}>
-            <Link href={item.catalogLink}>
-                <div className={styles.content}>
-                    <h2 className={styles.title}>{item.title}</h2>
-                    <h3 className={styles.subtitle}>{item.subtitle}</h3>
-                    <div
-                        className={`${styles.button} ${active ? styles.button_active : ''}`}
-                        style={{
-                            transition: !transition ? 'none' : `opacity ${speed}ms`,
-                        }}>
-                        <HighlightButton className={styles.highlight_button}>
-                            {t('show-compilation')}
-                        </HighlightButton>
-                    </div>
-                </div>
-                <div className={styles.background}>
-                    <Image
-                        src={item.imgUrlMobile}
-                        alt='Banner'
-                        fill
-                        className={`${styles.image} ${styles.image_mobile}`}
-                    />
-                    <Image src={item.imgUrl} alt='Banner' fill className={styles.image} />
-                </div>
-            </Link>
-        </article>
-    );
-};
+              transition: !transition ? 'none' : `opacity ${speed}ms`,
+            }}
+          >
+            <HighlightButton className={styles.highlight_button}>
+              {t('show-compilation')}
+            </HighlightButton>
+          </div>
+        </div>
+        <div className={styles.background}>
+          <Image
+            src={item.imgUrlMobile}
+            alt='Banner'
+            fill
+            className={`${styles.image} ${styles.image_mobile}`}
+          />
+          <Image src={item.imgUrl} alt='Banner' fill className={styles.image} />
+        </div>
+      </Link>
+    </article>
+  )
+}
 
-export default BannerCarouselItem;
+export default BannerCarouselItem
