daaab75aba48d923762124d2ab2ec3fe5011cde5
Marat Sattarov <sattarovmk@yandex.ru>
2023-05-28 02:12:59 +0500

    Fix (layouts) fixed PageLayout width

--- src/layouts/PageLayout/PageLayout.tsx
+++ src/layouts/PageLayout/PageLayout.tsx
@@ -1,24 +1,24 @@
-import Footer from '@/components/LayoutElements/Footer/Footer'
-import HeadModif from '@/components/HeadModif/HeadModif'
-import Header from '@/components/LayoutElements/Header/Header'
-import { IHead } from '@/types/head.interface'
-import style from './PageLayout.module.scss'
-import { FC, PropsWithChildren } from 'react'
-import { useSessionRefresh } from '@/hooks/useSessionRefresh'
+import Footer from '@/components/LayoutElements/Footer/Footer';
+import HeadModif from '@/components/HeadModif/HeadModif';
+import Header from '@/components/LayoutElements/Header/Header';
+import { IHead } from '@/types/head.interface';
+import style from './PageLayout.module.scss';
+import { FC, PropsWithChildren } from 'react';
+import { useSessionRefresh } from '@/hooks/useSessionRefresh';
 
 const PageLayout: FC<PropsWithChildren<IHead>> = ({ children, ...props }) => {
-  useSessionRefresh()
+    useSessionRefresh();
 
-  return (
-    <>
-      <HeadModif {...props} />
-      <div className={style.wrapper}>
-        <Header />
-        <main className={style.container}>{children}</main>
-        <Footer />
-      </div>
-    </>
-  )
-}
+    return (
+        <>
+            <HeadModif {...props} />
+            <div className={style.wrapper}>
+                <Header />
+                <main className={style.container}>{children}</main>
+                <Footer />
+            </div>
+        </>
+    );
+};
 
-export default PageLayout
+export default PageLayout;
