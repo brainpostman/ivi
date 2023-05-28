c1555c506730d0a1d7d6eb4f2d9ba4d504d29f49
Marat Sattarov <sattarovmk@yandex.ru>
2023-05-28 05:27:56 +0500

    Fix (FIlterSlider) fixed wrong step for url query

--- src/components/FilterBlock/FilterBlock.tsx
+++ src/components/FilterBlock/FilterBlock.tsx
@@ -1,191 +1,179 @@
-import { useFilter } from '@/hooks/useFilter'
-import { useTranslation } from 'next-i18next'
-import { useRouter } from 'next/router'
-import { FC, useEffect } from 'react'
-import { IoCloseOutline } from 'react-icons/io5'
-import FilterGenreCard from '../UI/Cards/FilterGenreCard/FilterGenreCard'
-import VioletButton from '../UI/Buttons/VioletButton/VioletButton'
-import style from './FilterBlock.module.scss'
-import FilterListBig from './FilterListBig/FilterListBig'
-import FilterSlider from './FilterSlider/FilterSlider'
-import FilterSuggest from './FilterSuggest/FilterSuggest'
-import { IStaffGetResponse } from '@/types/api/staffs.api.interface'
-import { IFilterGetResponse } from '@/types/api/filters.api.interface'
-import { useSetListParam } from '@/hooks/useSetListParam'
-import formatQueryLocale from '@/formatters/queryLocale.format'
-import { filterList } from '@/data/filters.data'
-import getFilterClassName from '@/utils/filterClassName.utils'
+import { useFilter } from '@/hooks/useFilter';
+import { useTranslation } from 'next-i18next';
+import { useRouter } from 'next/router';
+import { FC, useEffect } from 'react';
+import { IoCloseOutline } from 'react-icons/io5';
+import FilterGenreCard from '../UI/Cards/FilterGenreCard/FilterGenreCard';
+import VioletButton from '../UI/Buttons/VioletButton/VioletButton';
+import style from './FilterBlock.module.scss';
+import FilterListBig from './FilterListBig/FilterListBig';
+import FilterSlider from './FilterSlider/FilterSlider';
+import FilterSuggest from './FilterSuggest/FilterSuggest';
+import { IStaffGetResponse } from '@/types/api/staffs.api.interface';
+import { IFilterGetResponse } from '@/types/api/filters.api.interface';
+import { useSetListParam } from '@/hooks/useSetListParam';
+import formatQueryLocale from '@/formatters/queryLocale.format';
+import { filterList } from '@/data/filters.data';
+import getFilterClassName from '@/utils/filterClassName.utils';
 
 interface IProps {
-  genres: IFilterGetResponse[]
-  countries: IFilterGetResponse[]
-  directors: IStaffGetResponse[]
-  actors: IStaffGetResponse[]
-  className?: string
-  clearSort?: boolean
-  minYear: number
-  maxYear: number
-  minCountScore: number
-  maxCountScore: number
-  maxRating: number
+    genres: IFilterGetResponse[];
+    countries: IFilterGetResponse[];
+    directors: IStaffGetResponse[];
+    actors: IStaffGetResponse[];
+    className?: string;
+    clearSort?: boolean;
+    minYear: number;
+    maxYear: number;
+    minCountScore: number;
+    maxCountScore: number;
+    maxRating: number;
 }
 
 const FilterBlock: FC<IProps> = ({
-  genres,
-  countries,
-  directors,
-  actors,
-  className: propsClassName,
-  clearSort = true,
-  minYear,
-  maxYear,
-  minCountScore,
-  maxCountScore,
-  maxRating,
+    genres,
+    countries,
+    directors,
+    actors,
+    className: propsClassName,
+    clearSort = true,
+    minYear,
+    maxYear,
+    minCountScore,
+    maxCountScore,
+    maxRating,
 }) => {
-  const router = useRouter()
-  const { t } = useTranslation('movies')
+    const router = useRouter();
+    const { t } = useTranslation('movies');
 
-  const { expandTabFilter, getFilterData } = useFilter(filterList)
+    const { expandTabFilter, getFilterData } = useFilter(filterList);
 
-  const genreFilterData = getFilterData('genres')
-  const countryFilterData = getFilterData('countries')
-  const producerFilterData = getFilterData('director')
-  const actorFilterData = getFilterData('actor')
+    const genreFilterData = getFilterData('genres');
+    const countryFilterData = getFilterData('countries');
+    const producerFilterData = getFilterData('director');
+    const actorFilterData = getFilterData('actor');
 
-  const isAppliedFilters = Object.keys(router.query).length
+    const isAppliedFilters = Object.keys(router.query).length;
 
-  const { onClickListEl: onClickGenreCard } = useSetListParam(
-    genres.slice(0, 10).map(genre => ({ ...genre, isSelect: false })),
-    'genres',
-    {
-      queryFormatter: formatQueryLocale(
+    const { onClickListEl: onClickGenreCard } = useSetListParam(
+        genres.slice(0, 10).map((genre) => ({ ...genre, isSelect: false })),
         'genres',
-        'genres_en',
-        router.locale ?? 'ru'
-      ),
-    }
-  )
+        {
+            queryFormatter: formatQueryLocale('genres', 'genres_en', router.locale ?? 'ru'),
+        }
+    );
 
-  const { onClickListEl: onClickCountry } = useSetListParam(
-    countries.slice(0, 10).map(country => ({ ...country, isSelect: false })),
-    'countries'
-  )
+    const { onClickListEl: onClickCountry } = useSetListParam(
+        countries.slice(0, 10).map((country) => ({ ...country, isSelect: false })),
+        'countries'
+    );
 
-  const clearFilters = () => {
-    router.replace({ pathname: router.pathname, query: undefined }, undefined, {
-      shallow: true,
-    })
-  }
+    const clearFilters = () => {
+        router.replace({ pathname: router.pathname, query: undefined }, undefined, {
+            shallow: true,
+        });
+    };
 
-  useEffect(() => {
-    const copy = { ...router.query }
-    const paramKeys = Object.keys(copy)
+    useEffect(() => {
+        const copy = { ...router.query };
+        const paramKeys = Object.keys(copy);
 
-    if (!paramKeys.length) return
+        if (!paramKeys.length) return;
 
-    const filteredParamKeys = paramKeys.filter(
-      el => !['orderBy', 'order'].includes(el)
-    )
+        const filteredParamKeys = paramKeys.filter((el) => !['orderBy', 'order'].includes(el));
 
-    if (!filteredParamKeys.length && clearSort) {
-      clearFilters()
-    }
-  }, [router.query])
+        if (!filteredParamKeys.length && clearSort) {
+            clearFilters();
+        }
+    }, [router.query]);
 
-  return (
-    <section className={`${style.wrapper} ${propsClassName}`}>
-      <FilterListBig
-        filterData={genreFilterData}
-        list={genres}
-        carouselElementsView={5}
-        query='genres'
-      >
-        {genres.slice(0, 10).map(genre => (
-          <FilterGenreCard
-            key={genre.id}
-            onClick={onClickGenreCard(genre.name)}
-            className={getFilterClassName(
-              genre.name,
-              style.filter_card_active,
-              router.query.genres as string | undefined
-            )}
-          >
-            {genre.name}
-          </FilterGenreCard>
-        ))}
-      </FilterListBig>
+    return (
+        <section className={`${style.wrapper} ${propsClassName}`}>
+            <FilterListBig
+                filterData={genreFilterData}
+                list={genres}
+                carouselElementsView={5}
+                query='genres'>
+                {genres.slice(0, 10).map((genre) => (
+                    <FilterGenreCard
+                        key={genre.id}
+                        onClick={onClickGenreCard(genre.name)}
+                        className={getFilterClassName(
+                            genre.name,
+                            style.filter_card_active,
+                            router.query.genres as string | undefined
+                        )}>
+                        {genre.name}
+                    </FilterGenreCard>
+                ))}
+            </FilterListBig>
 
-      <FilterListBig
-        filterData={countryFilterData}
-        list={countries}
-        carouselElementsView={6}
-        carouselElementsMove={1}
-        query='countries'
-      >
-        {countries.slice(0, 10).map(country => (
-          <VioletButton
-            key={country.id}
-            onClick={onClickCountry(country.name)}
-            variant='secondary'
-            className={getFilterClassName(
-              country.name,
-              style.filter_card_active,
-              router.query.countries as string | undefined
-            )}
-          >
-            {country.name}
-          </VioletButton>
-        ))}
-      </FilterListBig>
+            <FilterListBig
+                filterData={countryFilterData}
+                list={countries}
+                carouselElementsView={6}
+                carouselElementsMove={1}
+                query='countries'>
+                {countries.slice(0, 10).map((country) => (
+                    <VioletButton
+                        key={country.id}
+                        onClick={onClickCountry(country.name)}
+                        variant='secondary'
+                        className={getFilterClassName(
+                            country.name,
+                            style.filter_card_active,
+                            router.query.countries as string | undefined
+                        )}>
+                        {country.name}
+                    </VioletButton>
+                ))}
+            </FilterListBig>
 
-      <FilterSuggest
-        filterData={producerFilterData}
-        closeModal={expandTabFilter('director')}
-        suggestList={directors}
-        placeholder={t('searches.director-placeholder')}
-        query='directors'
-      />
+            <FilterSuggest
+                filterData={producerFilterData}
+                closeModal={expandTabFilter('director')}
+                suggestList={directors}
+                placeholder={t('searches.director-placeholder')}
+                query='directors'
+            />
 
-      <FilterSuggest
-        filterData={actorFilterData}
-        closeModal={expandTabFilter('actor')}
-        suggestList={actors}
-        placeholder={t('searches.actor-placeholder')}
-        query='actors'
-      />
+            <FilterSuggest
+                filterData={actorFilterData}
+                closeModal={expandTabFilter('actor')}
+                suggestList={actors}
+                placeholder={t('searches.actor-placeholder')}
+                query='actors'
+            />
 
-      <FilterSlider
-        query={{ min: 'yearStart', max: 'yearEnd' }}
-        range={{ min: minYear, max: maxYear }}
-        title={t('sliders.years')}
-        step={1}
-      />
-      <FilterSlider
-        query='minCountScore'
-        title={t('sliders.scores')}
-        range={{ min: minCountScore, max: maxCountScore }}
-      />
-      <FilterSlider
-        query='ratingStart'
-        range={{ max: Math.floor(maxRating) }}
-        title={t('sliders.rating')}
-        step={0.1}
-      />
+            <FilterSlider
+                query={{ min: 'yearStart', max: 'yearEnd' }}
+                range={{ min: minYear, max: maxYear }}
+                title={t('sliders.years')}
+                step={1}
+            />
+            <FilterSlider
+                query='minCountScore'
+                title={t('sliders.scores')}
+                range={{ min: minCountScore, max: maxCountScore }}
+                step={1}
+            />
+            <FilterSlider
+                query='ratingStart'
+                range={{ max: Math.floor(maxRating) }}
+                title={t('sliders.rating')}
+                step={0.1}
+            />
 
-      <div
-        className={`${
-          !isAppliedFilters
-            ? style.clear_filters__deactive
-            : style.clear_filters
-        }`}
-        onClick={isAppliedFilters ? clearFilters : () => {}}
-      >
-        <IoCloseOutline />
-        <p>{t('reset-filters')}</p>
-      </div>
-    </section>
-  )
-}
+            <div
+                className={`${
+                    !isAppliedFilters ? style.clear_filters__deactive : style.clear_filters
+                }`}
+                onClick={isAppliedFilters ? clearFilters : () => {}}>
+                <IoCloseOutline />
+                <p>{t('reset-filters')}</p>
+            </div>
+        </section>
+    );
+};
 
-export default FilterBlock
+export default FilterBlock;
--- src/components/FilterBlock/FilterSlider/FilterSliderSingle/FilterSliderSingle.tsx
+++ src/components/FilterBlock/FilterSlider/FilterSliderSingle/FilterSliderSingle.tsx
@@ -1,58 +1,62 @@
-import ReactSlider from 'react-slider'
-import { ChangeEvent, FC, useState } from 'react'
-import { useSetStringParam } from '@/hooks/useSetStringParam'
-import Input from '@/components/UI/InputFields/Input/Input'
-import style from './FilterSliderSingle.module.scss'
-import styleParent from '../FilterSlider.module.scss'
+import ReactSlider from 'react-slider';
+import { ChangeEvent, FC, useState } from 'react';
+import { useSetStringParam } from '@/hooks/useSetStringParam';
+import Input from '@/components/UI/InputFields/Input/Input';
+import style from './FilterSliderSingle.module.scss';
+import styleParent from '../FilterSlider.module.scss';
 
 interface IProps {
-  query: string
-  title: string
-  min?: number
-  max: number
-  step?: number
+    query: string;
+    title: string;
+    min?: number;
+    max: number;
+    step?: number;
 }
 
 const FilterSliderSingle: FC<IProps> = ({ query, title, max, min, step }) => {
-  const [value, setValue] = useState(min || 0)
+    const [value, setValue] = useState(min || 0);
 
-  const onChangeValue = (event: ChangeEvent<HTMLInputElement>) => {
-    setValue(+event.target.value)
-  }
+    const onChangeValue = (event: ChangeEvent<HTMLInputElement>) => {
+        setValue(+event.target.value);
+    };
 
-  const { setUrl } = useSetStringParam(query, (min || 0).toString(), {
-    isNumber: true,
-    extraValues: [(min || 0).toString()],
-  })
+    const { setUrl } = useSetStringParam(query, (min || 0).toString(), {
+        isNumber: true,
+        extraValues: [(min || 0).toString()],
+    });
 
-  return (
-    <div>
-      <div className={style.wrapper_input}>
-        <Input
-          type='number'
-          placeholder={(min || 0).toString()}
-          value={value}
-          onChange={event => onChangeValue(event)}
-          className={style.input}
-        />
-        <p>{title}</p>
-      </div>
-      <ReactSlider
-        value={value}
-        onChange={value => {
-          setValue(value)
-        }}
-        min={min || 0}
-        max={max}
-        step={max * 0.01}
-        className={styleParent.slider}
-        thumbClassName={styleParent.thumb}
-        trackClassName={styleParent.track}
-        onAfterChange={value => setUrl(value)}
-        pearling
-      />
-    </div>
-  )
-}
+    return (
+        <div>
+            <div className={style.wrapper_input}>
+                <Input
+                    type='number'
+                    placeholder={(min || 0).toString()}
+                    value={value}
+                    onChange={(event) => onChangeValue(event)}
+                    className={style.input}
+                />
+                <p>{title}</p>
+            </div>
+            <ReactSlider
+                value={value}
+                onChange={(value) => {
+                    setValue(value);
+                }}
+                min={min || 0}
+                max={max}
+                step={step}
+                className={styleParent.slider}
+                thumbClassName={styleParent.thumb}
+                trackClassName={styleParent.track}
+                onAfterChange={(value) =>
+                    setUrl(
+                        value.toString().indexOf('.') === -1 ? Math.round(value) : value.toFixed(1)
+                    )
+                }
+                pearling
+            />
+        </div>
+    );
+};
 
-export default FilterSliderSingle
+export default FilterSliderSingle;
